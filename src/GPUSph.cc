f3f1088519e4a466648008bc13f66b2f47776ecc
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2012-06-01 14:19:53 +0200

    Only save normals when it's enabled

--- src/GPUSph.cc
+++ src/GPUSph.cc
@@ -424,7 +424,8 @@ void display()
 			if (problem->m_simparams.vorticity)
 				psystem->getArray(ParticleSystem::VORTICITY, need_write);
 			// DEBUG
-			psystem->getArray(ParticleSystem::NORMALS, need_write);
+			if (problem->m_simparams.savenormals)
+				psystem->getArray(ParticleSystem::NORMALS, need_write);
 
 			psystem->writeToFile();
 			#define ti timingInfo
