19218063e6f252b902a53c18fca858cfe04f564b
alexis <alexis.herault@cnam.frr>
2012-07-24 18:25:38 +0200

    First Kepler optimisation

--- src/forces.cu
+++ src/forces.cu
@@ -195,7 +195,7 @@ forces(	float4*			pos,
 	if (visctype == SPSVISC) {	// thread per particle
 		int numThreads = min(BLOCK_SIZE_SPS, numParticles);
 		int numBlocks = (int) ceil(numParticles / (float) numThreads);
-		#if (__COMPUTE__ >= 20)
+		#if (__COMPUTE__ == 20)
 		dummy_shared = 2560;
 		#endif
 		if (periodicbound) {
@@ -222,7 +222,7 @@ forces(	float4*			pos,
 	// thread per particle
 	int numThreads = min(BLOCK_SIZE_FORCES, numParticles);
 	int numBlocks = (int) ceil(numParticles / (float) numThreads);		
-	#if (__COMPUTE__ >= 20)
+	#if (__COMPUTE__ == 20)
 	if (visctype == SPSVISC)
 		dummy_shared = 3328 - dtadapt*BLOCK_SIZE_FORCES*4;
 	else
@@ -307,7 +307,7 @@ shepard(float4*		pos,
 	CUDA_SAFE_CALL(cudaBindTexture(0, infoTex, info, numParticles*sizeof(particleinfo)));
 	
 	// execute the kernel
-	#if (__COMPUTE__ >= 20)
+	#if (__COMPUTE__ == 20)
 	dummy_shared = 2560;
 	#endif
 	if (periodicbound) {
@@ -358,7 +358,7 @@ mls(float4*		pos,
 	CUDA_SAFE_CALL(cudaBindTexture(0, infoTex, info, numParticles*sizeof(particleinfo)));
 
 	// execute the kernel		
-	#if (__COMPUTE__ >= 20)
+	#if (__COMPUTE__ == 20)
 	dummy_shared = 2560;
 	#endif
 	if (periodicbound) {
