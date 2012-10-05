689dc8d1448ccbb3bf271a63c0ee658b4e926a5d
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2012-10-05 15:49:49 +0200

    compute_select.h should not exist
    The compute capability selector is compute_select.opt, not .h, and the
    latter is a spurious file that shouldn't exist. Expunge and fix
    cudautil.cu accordingly.

--- src/compute_select.h
+++
@@ -1,2 +0,0 @@
-/* Define the compute capability GPU code was compiled for. */
-#define COMPUTE 12
--- src/cudautil.cu
+++ src/cudautil.cu
@@ -2,7 +2,7 @@
 #include <cuda_gl_interop.h>
 
 #include "cudautil.cuh"
-#include "compute_select.h"
+#include "compute_select.opt"
 
 cudaDeviceProp checkCUDA(const Options &options)
 {
