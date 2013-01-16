47ff98d88e9c148a4b1019773b2312b5c4ef3170
Eugenio Rustico <eugenio.rustico@baw.de>
2013-01-16 13:42:21 +0100

    Draft of structure for main()

--- src/main.cc
+++ src/main.cc
@@ -5,8 +5,26 @@
  *      Author: rustico
  */
 
+#include "Options.h"
+#include "GlobalData.h"
 
 int newMain() {
 	// stub
+int newMain(int argc, char** argv) {
+	// Command line options
+	Options clOptions;
+
+	// GlobalData, to be read potentially by everyone
+	GlobalData gdata;
+
+	// TODO: check uint = 2 short
+	// TODO: catch signal SIGINT et al.
+
+	// TODO: parse_options(argc, argv);
+	// TODO: check options
+
+	// TODO: equivalent of GPUThread::runMultiGPU(&clOptions, &cdata);
+
+	return 0;
 }
 
