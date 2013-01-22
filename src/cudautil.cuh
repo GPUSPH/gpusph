9a341d5032db07ed82b27b79d7ee34e1695b1353
Eugenio Rustico <eugenio.rustico@baw.de>
2013-01-22 11:01:57 +0100

    New overloaded checkCUDA

--- src/cudautil.cuh
+++ src/cudautil.cuh
@@ -1,8 +1,11 @@
 #ifndef _CUDAUTIL_CUH_
 #define _CUDAUTIL_CUH_
 
+#include "GlobalData.h"
 #include "Options.h"
 #include "cuda_call.h"
+
+cudaDeviceProp checkCUDA(const GlobalData* gdata, uint devnum);
 
 cudaDeviceProp checkCUDA(const Options&);
 
