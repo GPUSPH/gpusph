a9317e51a965faf0d7c5e3360b840ddca33313b8
Eugenio Rustico <eugenio.rustico@baw.de>
2013-02-01 16:30:58 +0100

    Implemented GPUWorker::uploadSubdomains()

--- src/GPUWorker.cc
+++ src/GPUWorker.cc
@@ -245,8 +245,22 @@ void GPUWorker::deallocateDeviceBuffers() {
 	// here: dem device buffers?
 }
 
+// upload subdomain, just allocated and sorted by main thread
 void GPUWorker::uploadSubdomains() {
-	// upload subdomain, just allocated and sorted by main thread
+	// indices
+	uint myDevNum = devnum; // global device number
+	uint firstInnerParticle	= gdata->s_hStartPerDevice[myDevNum];
+	uint howManyParticles	= gdata->s_hPartsPerDevice[myDevNum];
+
+	size_t _size = 0;
+
+	// memcpys - recalling GPU arrays are double buffered
+	_size = howManyParticles * sizeof( m_hPos[0] ); // float4
+	CUDA_SAFE_CALL(cudaMemcpy(m_dPos[0], m_hPos, _size, cudaMemcpyHostToDevice));
+	_size = howManyParticles * sizeof( m_hVel[0] ); // float4
+	CUDA_SAFE_CALL(cudaMemcpy(m_dVel[0], m_hVel, _size, cudaMemcpyHostToDevice));
+	_size = howManyParticles * sizeof( m_hInfo[0] ); // particleInfo
+	CUDA_SAFE_CALL(cudaMemcpy(m_dInfo[0], m_hInfo, _size, cudaMemcpyHostToDevice));
 }
 
 // create a compact device map, for this device, from the global one,
