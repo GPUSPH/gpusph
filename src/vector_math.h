398725e223edd93c1e74df9cb56df7dcd636609d
Alexis Herault <alexis.herault@cnam.fr>
2013-05-23 17:30:04 -0400

    WIP: removed abs(int3) and changed __inline__ to __forceinline__

--- src/vector_math.h
+++ src/vector_math.h
@@ -42,13 +42,13 @@ inline float rsqrtf(float x)
 ////////////////////////////////////////////////////////////////////////////////
 
 // lerp
-static __inline__ __device__ __host__ float lerp(const float &a, const float &b, const float &t)
+static __device__ __forceinline__ __host__ float lerp(const float &a, const float &b, const float &t)
 {
 	return a + t*(b-a);
 }
 
 // clamp
-static __inline__ __device__ __host__ float clamp(const float &f, const float &a, const float &b)
+static __device__ __forceinline__ __host__ float clamp(const float &f, const float &a, const float &b)
 {
 	return fmaxf(a, fminf(f, b));
 }
@@ -193,18 +193,18 @@ static __inline__ __host__ __device__ void operator/=(float2 &a, const float &s)
 }
 
 // lerp
-static __inline__ __device__ __host__ float2 lerp(const float2 &a, const float2 &b, const float &t)
+static __device__ __forceinline__ __host__ float2 lerp(const float2 &a, const float2 &b, const float &t)
 {
 	return a + t*(b-a);
 }
 
 // clamp
-static __inline__ __device__ __host__ float2 clamp(const float2 &v, const float &a, const float &b)
+static __device__ __forceinline__ __host__ float2 clamp(const float2 &v, const float &a, const float &b)
 {
 	return make_float2(clamp(v.x, a, b), clamp(v.y, a, b));
 }
 
-static __inline__ __device__ __host__ float2 clamp(const float2 &v, const float2 &a, const float2 &b)
+static __device__ __forceinline__ __host__ float2 clamp(const float2 &v, const float2 &a, const float2 &b)
 {
 	return make_float2(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y));
 }
@@ -409,18 +409,18 @@ static __inline__ __host__ __device__ void operator/=(float3 &a, const float &s)
 }
 
 // lerp
-static __inline__ __device__ __host__ float3 lerp(const float3 &a, const float3 &b, const float &t)
+static __device__ __forceinline__ __host__ float3 lerp(const float3 &a, const float3 &b, const float &t)
 {
 	return a + t*(b-a);
 }
 
 // clamp
-static __inline__ __device__ __host__ float3 clamp(const float3 &v, const float &a, const float &b)
+static __device__ __forceinline__ __host__ float3 clamp(const float3 &v, const float &a, const float &b)
 {
 	return make_float3(clamp(v.x, a, b), clamp(v.y, a, b), clamp(v.z, a, b));
 }
 
-static __inline__ __device__ __host__ float3 clamp(const float3 &v, const float3 &a, const float3 &b)
+static __device__ __forceinline__ __host__ float3 clamp(const float3 &v, const float3 &a, const float3 &b)
 {
 	return make_float3(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z));
 }
@@ -796,18 +796,18 @@ static __inline__ __host__ __device__ void operator/=(float4 &a, float s)
 }
 
 // lerp
-static __inline__ __device__ __host__ float4 lerp(const float4 &a, const float4 &b, const float &t)
+static __device__ __forceinline__ __host__ float4 lerp(const float4 &a, const float4 &b, const float &t)
 {
 	return a + t*(b-a);
 }
 
 // clamp
-static __inline__ __device__ __host__ float4 clamp(const float4 &v, const float &a, const float &b)
+static __device__ __forceinline__ __host__ float4 clamp(const float4 &v, const float &a, const float &b)
 {
 	return make_float4(clamp(v.x, a, b), clamp(v.y, a, b), clamp(v.z, a, b), clamp(v.w, a, b));
 }
 
-static __inline__ __device__ __host__ float4 clamp(const float4 &v, const float4 &a, const float4 &b)
+static __device__ __forceinline__ __host__ float4 clamp(const float4 &v, const float4 &a, const float4 &b)
 {
 	return make_float4(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z), clamp(v.w, a.w, b.w));
 }
@@ -950,26 +950,19 @@ static __inline__ __host__ __device__ void operator/=(int3 &a, const int &s)
 }
 
 // clamp
-static __inline__ __device__ __host__ int clamp(const int &f, const int &a, const int &b)
+static __device__ __forceinline__ __host__ int clamp(const int &f, const int &a, const int &b)
 {
 	return max(a, min(f, b));
 }
 
-static __inline__ __device__ __host__ int3 clamp(const int3 &v, const int &a, const int &b)
+static __device__ __forceinline__ __host__ int3 clamp(const int3 &v, const int &a, const int &b)
 {
 	return make_int3(clamp(v.x, a, b), clamp(v.y, a, b), clamp(v.z, a, b));
 }
 
-static __inline__ __device__ __host__ int3 clamp(const int3 &v, const int3 &a, const int3 &b)
+static __device__ __forceinline__ __host__ int3 clamp(const int3 &v, const int3 &a, const int3 &b)
 {
 	return make_int3(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z));
-}
-
-
-// abs
-static __inline__ __device__ __host__ int3 abs(const int3 &a)
-{
-	return make_int3(abs(a.x), abs(a.y), abs(a.z));
 }
 
 // uint3 functions
@@ -1072,17 +1065,17 @@ static __inline__ __host__ __device__ void operator/=(uint3 &a, const uint &s)
 }
 
 // clamp
-static __inline__ __device__ __host__ uint clamp(const uint &f, const uint &a, const uint &b)
+static __device__ __forceinline__ __host__ uint clamp(const uint &f, const uint &a, const uint &b)
 {
 	return max(a, min(f, b));
 }
 
-static __inline__ __device__ __host__ uint3 clamp(const uint3 &v, const uint &a, const uint &b)
+static __device__ __forceinline__ __host__ uint3 clamp(const uint3 &v, const uint &a, const uint &b)
 {
 	return make_uint3(clamp(v.x, a, b), clamp(v.y, a, b), clamp(v.z, a, b));
 }
 
-static __inline__ __device__ __host__ uint3 clamp(const uint3 &v, const uint3 &a, const uint3 &b)
+static __device__ __forceinline__ __host__ uint3 clamp(const uint3 &v, const uint3 &a, const uint3 &b)
 {
 	return make_uint3(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z));
 }
