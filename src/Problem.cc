208c2ba1f7224211e85e066df40afee550e6767b
vorobyev <vorobyev.alexander@gmail.com>
2013-07-03 15:26:59 +0200

    Set initial values of k and epsilon depending on model parameters
    The values are defined as:
    k0 = (0.002 * C0)^2, where C0 - speed of sound
    e0 = 0.16 * k0^(3/2) / Lm, where Lm = max(2*dp, 1e-5)

--- src/Problem.cc
+++ src/Problem.cc
@@ -380,8 +380,12 @@ Problem::draw_axis()
 void
 Problem::init_keps(float* k, float* e, int numpart, particleinfo* info)
 {
+	const float Lm = max(2*m_deltap, 1e-5f);
+	const float k0 = pow(0.002f*m_physparams.sscoeff[0], 2);
+	const float e0 = 0.16f*pow(k0, 1.5f)/Lm;
+
 	for (uint i = 0; i < numpart; i++) {
-		k[i] = 0.01f;
-		e[i] = 0.008f;
+		k[i] = k0;
+		e[i] = e0;
 	}
 }
