0abb8eccfb56a91e336c086f06b26bb6054ef1d0
Eugenio Rustico <eugenio.rustico@baw.de>
2013-10-14 16:28:49 +0200

    Option only_internals also used for surface particles kernel

--- src/GPUWorker.cc
+++ src/GPUWorker.cc
@@ -1580,7 +1580,7 @@ void GPUWorker::kernel_surfaceParticles()
 					 m_dInfo[gdata->currentInfoRead],
 					 m_dInfo[gdata->currentInfoWrite],
 					 m_dNeibsList,
-					 m_numParticles,
+					 (gdata->only_internal ? m_particleRangeEnd : m_numParticles),
 					 m_simparams->slength,
 					 m_simparams->kerneltype,
 					 m_simparams->influenceRadius,
