fb7c0253629c5ad7281f3ed989e9d947d9185739
Eugenio Rustico <eugenio.rustico@baw.de>
2013-11-20 14:39:04 +0100

    Cleanup in print_usage()

--- src/main.cc
+++ src/main.cc
@@ -23,21 +23,19 @@ void print_usage() {
 	cerr << "Syntax: " << endl;
 	cerr << "\tGPUSPH [--device n[,n...]] [--dem dem_file] [--deltap VAL] [--tend VAL]\n";
 	cerr << "\t       [--pthreads] [--dir directory] [--nosave] [--nobalance] [--nopause]\n";
-	cerr << "\t       [--cpuonly VAL [--single]]\n";
-	cerr << "\t       [--alloc-max] [--lb-threshold VAL] [--cpuonly VAL]\n";
 	cerr << "\tGPUSPH --help\n\n";
 	cerr << " --device n[,n...] : Use device number n; runs multi-gpu if multiple n are given\n";
 	cerr << " --dem : Use given DEM (if problem supports it)\n";
 	cerr << " --deltap : Use given deltap (VAL is cast to float)\n";
 	cerr << " --tend: Break at given time (VAL is cast to float)\n";
 	cerr << " --dir : Use given directory for dumps instead of date-based one\n";
-	cerr << " --pthreads : Force use of threads even if single GPU\n";
+	//cerr << " --pthreads : Force use of threads even if single GPU\n";
 	cerr << " --nosave : Disable all file dumps but the last\n";
-	cerr << " --nobalance : Disable dynamic load balancing\n";
-	cerr << " --alloc-max : Alloc total number of particles for every device\n";
-	cerr << " --lb-threshold : Set custom LB activation threshold (VAL is cast to float)\n";
-	cerr << " --cpuonly : Simulates on the CPU using VAL pthreads\n";
-	cerr << " --single : Computes fluid-fluid interactions once per pair\n";
+	//cerr << " --nobalance : Disable dynamic load balancing\n";
+	//cerr << " --alloc-max : Alloc total number of particles for every device\n";
+	//cerr << " --lb-threshold : Set custom LB activation threshold (VAL is cast to float)\n";
+	//cerr << " --cpuonly : Simulates on the CPU using VAL pthreads\n";
+	//cerr << " --single : Computes fluid-fluid interactions once per pair\n";
 	cerr << " --help: Show this help and exit\n";
 	//exit(-1);
 }
