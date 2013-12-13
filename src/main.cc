a5f858fb2e5c1328ec429692cf48a2c8bec6d33a
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2013-12-13 18:50:11 +0100

    First pass of GPUSPH cleanup
    Removed all the old stuff that has been moved to main.

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,53 @@
-/*
- * main.cc
+/*  Copyright 2011-2013 Alexis Herault, Giuseppe Bilotta, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+
+    Istituto Nazionale di Geofisica e Vulcanologia
+        Sezione di Catania, Catania, Italy
+
+    Università di Catania, Catania, Italy
+
+    Johns Hopkins University, Baltimore, MD
+
+    This file is part of GPUSPH.
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*! \mainpage GPUSPH Developer's documentation
  *
- *  Created on: Jan 16, 2013
- *      Author: rustico
+ *
+ * \section intro_sec  Introduction
+ *
+ * GPUSPH is a CUDA-based 3D SPH simulator (STUB).
+ *
+ * \section compile_sec Compiling and installing
+ *
+ * See "make help" (STUB).
+ *
+ * \section quick_links Internal links
+ * - \ref main \n
+ * - ParticleSystem
+ *
+ * \section links Links
+ * - <a href="http://www.stack.nl/~dimitri/doxygen/manual.html">Complete Doxygen manual</a>
+ * - <a href="http://www.nvidia.com/object/cuda_gpus.html">GPUs and compute capabilites</a>
+ *
+ *
+ * GPUSPH is a CUDA-based 3D SPH simulator (FIX).
+ *
+ * This document was generated with Doxygen.\n
+ *
  */
 
 // endl, cerr, etc.
