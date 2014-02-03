GPUSPH/gpusph

commit 3d77ffba4305cc62d42f06808600dafaaa6d2195

Fix: copy_to_array() now supports 64 bit hashKey
Problem::calc_localpos_and_hash() has been extended to correctly
create a 64 bit hash. It now requires also the particleInfo. This
has been fixes in all the problems.

Note: from now on, calc_localpos_and_hash() must be called *after*
filling the particleInfo.

src/InputProblem.cc
@@ -271,9 +271,9 @@ void InputProblem::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, v
 	for (uint i = 0; i < n_parts; i++) {
 		//float rho = density(H - buf[i].Coords_2, 0);
 		float rho = m_physparams.rho0[0];
-		calc_localpos_and_hash(Point(buf[i].Coords_0, buf[i].Coords_1, buf[i].Coords_2, rho*buf[i].Volume), pos[i], hash[i]);
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 		info[i] = make_particleinfo(FLUIDPART, 0, i);
+		calc_localpos_and_hash(Point(buf[i].Coords_0, buf[i].Coords_1, buf[i].Coords_2, rho*buf[i].Volume), info[i], pos[i], hash[i]);
 	}
 	uint j = n_parts;
 	std::cout << "Fluid part mass: " << pos[j-1].w << "\n";
@@ -282,9 +282,9 @@ void InputProblem::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, v
 		std::cout << "Vertex parts: " << n_vparts << "\n";
 		for (uint i = j; i < j + n_vparts; i++) {
 			float rho = density(H - buf[i].Coords_2, 0);
-			calc_localpos_and_hash(Point(buf[i].Coords_0, buf[i].Coords_1, buf[i].Coords_2, rho*buf[i].Volume), pos[i], hash[i]);
 			vel[i] = make_float4(0, 0, 0, rho);
 			info[i] = make_particleinfo(VERTEXPART, 0, i);
+			calc_localpos_and_hash(Point(buf[i].Coords_0, buf[i].Coords_1, buf[i].Coords_2, rho*buf[i].Volume), info[i], pos[i], hash[i]);
 		}
 		j += n_vparts;
 		std::cout << "Vertex part mass: " << pos[j-1].w << "\n";
@@ -293,9 +293,9 @@ void InputProblem::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, v
 	if(n_bparts) {
 		std::cout << "Boundary parts: " << n_bparts << "\n";
 		for (uint i = j; i < j + n_bparts; i++) {
-			calc_localpos_and_hash(Point(buf[i].Coords_0, buf[i].Coords_1, buf[i].Coords_2, 0.0), pos[i], hash[i]);
 			vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 			info[i] = make_particleinfo(BOUNDPART, 0, i);
+			calc_localpos_and_hash(Point(buf[i].Coords_0, buf[i].Coords_1, buf[i].Coords_2, 0.0), info[i], pos[i], hash[i]);
 			vertices[i].x = buf[i].VertexParticle1;
 			vertices[i].y = buf[i].VertexParticle2;
 			vertices[i].z = buf[i].VertexParticle3;
@@ -314,9 +314,9 @@ void InputProblem::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, v
 	if (test_points.size()) {
 		std::cout << "\nTest points: " << test_points.size() << "\n";
 		for (uint i = j; i < j+test_points.size(); i++) {
-			calc_localpos_and_hash(test_points[i-j], pos[i], hash[i]);
 			vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 			info[i]= make_particleinfo(TESTPOINTSPART, 0, i);
+			calc_localpos_and_hash(test_points[i-j], info[i], pos[i], hash[i]);
 		}
 		j += test_points.size();
 		std::cout << "Test point mass:" << pos[j-1].w << "\n";

src/StillWater.cc
@@ -179,28 +179,27 @@ void StillWater::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, has
 {
 	std::cout << "Boundary parts: " << boundary_parts.size() << "\n";
 	for (uint i = 0; i < boundary_parts.size(); i++) {
-		calc_localpos_and_hash(boundary_parts[i], pos[i], hash[i]);
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 		info[i] = make_particleinfo(BOUNDPART, 0, i);
+		calc_localpos_and_hash(boundary_parts[i], info[i], pos[i], hash[i]);
 	}
 	int j = boundary_parts.size();
 	std::cout << "Boundary part mass: " << pos[j-1].w << "\n";
 
 	std::cout << "Fluid parts: " << parts.size() << "\n";
 	for (uint i = j; i < j + parts.size(); i++) {
-		calc_localpos_and_hash(parts[i-j], pos[i], hash[i]);
 		float rho = density(H - pos[i].z, 0);
 		vel[i] = make_float4(0, 0, 0, rho);
 		info[i] = make_particleinfo(FLUIDPART, 0, i);
+		calc_localpos_and_hash(parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += parts.size();
 	std::cout << "Fluid part mass: " << pos[j-1].w << "\n";
 }
 
-void StillWater::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, vertexinfo *vertices, float4 *boundelm, uint* hash)
+void StillWater::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, vertexinfo *vertices, float4 *boundelm, hashKey* hash)
 {
 	float4 localpos;
-	uint hashvalue;
 
 	copy_to_array(pos, vel, info, hash);
 
@@ -208,12 +207,11 @@ void StillWater::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, ver
 
 	std::cout << "Vertex parts: " << vertex_parts.size() << "\n";
 	for (uint i = j; i < j + vertex_parts.size(); i++) {
-		calc_localpos_and_hash(vertex_parts[i-j], localpos, hashvalue);
 		pos[i] = localpos;
 		float rho = density(H - pos[i].z, 0);
 		vel[i] = make_float4(0, 0, 0, rho);
 		info[i] = make_particleinfo(VERTEXPART, 0, i);
-		hash[i] = hashvalue;
+		calc_localpos_and_hash(vertex_parts[i-j], info[i], localpos, hash[i]);
 	}
 	j += vertex_parts.size();
 	std::cout << "Vertex part mass: " << pos[j-1].w << "\n";

src/DamBreak3D.cc
@@ -198,16 +198,14 @@ void DamBreak3D::copy_planes(float4 *planes, float *planediv)
 
 void DamBreak3D::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, hashKey* hash)
 {
-	float4 localpos;
-	uint hashvalue;
 	int j = 0;
 
 	if(boundary_parts.size()){
 		std::cout << "Boundary parts: " << boundary_parts.size() << "\n";
 		for (uint i = 0; i < boundary_parts.size(); i++) {
-			calc_localpos_and_hash(boundary_parts[i], pos[i], hash[i]);
 			vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 			info[i]= make_particleinfo(BOUNDPART,0,i);
+			calc_localpos_and_hash(boundary_parts[i], info[i], pos[i], hash[i]);
 		}
 		j = boundary_parts.size();
 		std::cout << "Boundary part mass:" << pos[j-1].w << "\n";
@@ -217,9 +215,9 @@ void DamBreak3D::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, has
 	if (test_points.size()) {
 		std::cout << "\nTest points: " << test_points.size() << "\n";
 		for (uint i = 0; i < test_points.size(); i++) {
-			calc_localpos_and_hash(test_points[i], pos[i], hash[i]);
 			vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 			info[i]= make_particleinfo(TESTPOINTSPART, 0, i);
+			calc_localpos_and_hash(test_points[i], info[i], pos[i], hash[i]);
 		}
 		j += test_points.size();
 		std::cout << "Test point mass:" << pos[j-1].w << "\n";
@@ -227,18 +225,18 @@ void DamBreak3D::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, has
 
 	std::cout << "Obstacle parts: " << obstacle_parts.size() << "\n";
 	for (uint i = j; i < j + obstacle_parts.size(); i++) {
-		calc_localpos_and_hash(obstacle_parts[i-j], pos[i], hash[i]);
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 		info[i]= make_particleinfo(BOUNDPART,1,i);
+		calc_localpos_and_hash(obstacle_parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += obstacle_parts.size();
 	std::cout << "Obstacle part mass:" << pos[j-1].w << "\n";
 
 	std::cout << "Fluid parts: " << parts.size() << "\n";
 	for (uint i = j; i < j + parts.size(); i++) {
-		calc_localpos_and_hash(parts[i-j], pos[i], hash[i]);
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 		info[i]= make_particleinfo(FLUIDPART,0,i);
+		calc_localpos_and_hash(parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += parts.size();
 	std::cout << "Fluid part mass:" << pos[j-1].w << "\n";

src/DamBreakGate.cc
@@ -210,53 +210,38 @@ int DamBreakGate::fill_parts()
 
 void DamBreakGate::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, hashKey* hash)
 {
-	float4 localpos;
-	uint hashvalue;
-
 	std::cout << "Boundary parts: " << boundary_parts.size() << "\n";
 	for (uint i = 0; i < boundary_parts.size(); i++) {
-		calc_localpos_and_hash(boundary_parts[i], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(BOUNDPART,0,i);
+		info[i] = make_particleinfo(BOUNDPART,0,i);
+		calc_localpos_and_hash(boundary_parts[i], info[i], pos[i], hash[i]);
 	}
 	int j = boundary_parts.size();
 	std::cout << "Boundary part mass:" << pos[j-1].w << "\n";
 
 	std::cout << "Gate parts: " << gate_parts.size() << "\n";
 	for (uint i = j; i < j + gate_parts.size(); i++) {
-		calc_localpos_and_hash(gate_parts[i-j], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(GATEPART,0,i);
+		info[i] = make_particleinfo(GATEPART,0,i);
+		calc_localpos_and_hash(gate_parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += gate_parts.size();
 	std::cout << "Gate part mass:" << pos[j-1].w << "\n";
 
 	std::cout << "Obstacle parts: " << obstacle_parts.size() << "\n";
 	for (uint i = j; i < j + obstacle_parts.size(); i++) {
-		calc_localpos_and_hash(obstacle_parts[i-j], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(BOUNDPART,1,i);
+		info[i] = make_particleinfo(BOUNDPART,1,i);
+		calc_localpos_and_hash(obstacle_parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += obstacle_parts.size();
 	std::cout << "Obstacle part mass:" << pos[j-1].w << "\n";
 
 	std::cout << "Fluid parts: " << parts.size() << "\n";
 	for (uint i = j; i < j + parts.size(); i++) {
-		calc_localpos_and_hash(parts[i-j], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(FLUIDPART,0,i);
+		info[i] = make_particleinfo(FLUIDPART,0,i);
+		calc_localpos_and_hash(parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += parts.size();
 	std::cout << "Fluid part mass:" << pos[j-1].w << "\n";

src/OdeObjects.cc
@@ -219,17 +219,11 @@ void OdeObjects::ODE_near_callback(void *data, dGeomID o1, dGeomID o2)
 
 void OdeObjects::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, hashKey* hash)
 {
-	float4 localpos;
-	uint hashvalue;
-
 	std::cout << "Boundary parts: " << boundary_parts.size() << "\n";
 	for (uint i = 0; i < boundary_parts.size(); i++) {
-		calc_localpos_and_hash(boundary_parts[i], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(BOUNDPART, 0, i);
+		info[i] = make_particleinfo(BOUNDPART, 0, i);
+		calc_localpos_and_hash(boundary_parts[i], info[i], pos[i], hash[i]);
 	}
 	int j = boundary_parts.size();
 	std::cout << "Boundary part mass:" << pos[j-1].w << "\n";
@@ -238,12 +232,9 @@ void OdeObjects::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, has
 		PointVect & rbparts = get_ODE_body(k)->GetParts();
 		std::cout << "Rigid body " << k << ": " << rbparts.size() << " particles ";
 		for (uint i = j; i < j + rbparts.size(); i++) {
-			calc_localpos_and_hash(rbparts[i - j], localpos, hashvalue);
-
-			pos[i] = localpos;
-			hash[i] = hashvalue;
 			vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-			info[i]= make_particleinfo(OBJECTPART, k, i - j);
+			info[i] = make_particleinfo(OBJECTPART, k, i - j);
+			calc_localpos_and_hash(rbparts[i - j], info[i], pos[i], hash[i]);
 		}
 		j += rbparts.size();
 		std::cout << ", part mass: " << pos[j-1].w << "\n";
@@ -251,24 +242,18 @@ void OdeObjects::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, has
 
 	std::cout << "Obstacle parts: " << obstacle_parts.size() << "\n";
 	for (uint i = j; i < j + obstacle_parts.size(); i++) {
-		calc_localpos_and_hash(obstacle_parts[i-j], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(BOUNDPART, 1, i);
+		info[i] = make_particleinfo(BOUNDPART, 1, i);
+		calc_localpos_and_hash(obstacle_parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += obstacle_parts.size();
 	std::cout << "Obstacle part mass:" << pos[j-1].w << "\n";
 
 	std::cout << "Fluid parts: " << parts.size() << "\n";
 	for (uint i = j; i < j + parts.size(); i++) {
-		calc_localpos_and_hash(parts[i-j], localpos, hashvalue);
-
-		pos[i] = localpos;
-		hash[i] = hashvalue;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
-		info[i]= make_particleinfo(FLUIDPART, 0, i);
+		info[i] = make_particleinfo(FLUIDPART, 0, i);
+		calc_localpos_and_hash(parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += parts.size();
 	std::cout << "Fluid part mass:" << pos[j-1].w << "\n";

src/OpenChannel.cc
@@ -129,28 +129,19 @@ int OpenChannel::fill_parts()
 
 void OpenChannel::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, hashKey* hash)
 {
-	float4 localpos;
-	uint hashvalue;
-
 	std::cout << "Boundary parts: " << boundary_parts.size() << "\n";
 	for (uint i = 0; i < boundary_parts.size(); i++) {
-		calc_localpos_and_hash(boundary_parts[i], localpos, hashvalue);
-
-		pos[i] = localpos;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 		info[i]= make_particleinfo(BOUNDPART,0,i);
-		hash[i] = hashvalue;
+		calc_localpos_and_hash(boundary_parts[i], info[i], pos[i], hash[i]);
 	}
 	int j = boundary_parts.size();
 
 	std::cout << "Fluid parts: " << parts.size() << "\n";
 	for (uint i = j; i < j + parts.size(); i++) {
-		calc_localpos_and_hash(parts[i-j], localpos, hashvalue);
-
-		pos[i] = localpos;
 		vel[i] = make_float4(0, 0, 0, m_physparams.rho0[0]);
 		info[i]= make_particleinfo(FLUIDPART,0,i);
-		hash[i] = hashvalue;
+		calc_localpos_and_hash(parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += parts.size();
 	std::cout << "Fluid part mass:" << pos[j-1].w << "\n";

src/Problem.cc
@@ -757,10 +757,17 @@ Problem::calc_grid_hash(int3 gridPos)
 
 
 void
-Problem::calc_localpos_and_hash(const Point& pos, float4& localpos, hashKey& hash)
+Problem::calc_localpos_and_hash(const Point& pos, const particleinfo& info, float4& localpos, hashKey& hash)
 {
 	int3 gridPos = calc_grid_pos(pos);
 	hash = calc_grid_hash(gridPos);
+
+	// if hashkey is long, use long hash (cell hash + particle ID)
+	if (HASH_KEY_SIZE >= 64) {
+		hash <<= GRIDHASH_BITSHIFT;
+		hash |= id(info);
+	}
+
 	localpos.x = float(pos(0) - m_origin.x - (gridPos.x + 0.5)*m_cellsize.x);
 	localpos.y = float(pos(1) - m_origin.y - (gridPos.y + 0.5)*m_cellsize.y);
 	localpos.z = float(pos(2) - m_origin.z - (gridPos.z + 0.5)*m_cellsize.z);

src/Problem.h
@@ -207,7 +207,7 @@ class Problem {
 
 		uint calc_grid_hash(int3);
 
-		void calc_localpos_and_hash(const Point&, float4&, hashKey&);
+		void calc_localpos_and_hash(const Point&, const particleinfo&, float4&, hashKey&);
 
 		const SimParams *get_simparams(void) const
 		{

commit c2d8e6b62b40c562b36429bdc8a07eb29e49f1e0

StillWater fix after the calc_localpos_and_hash changes
Commit 3d77ffba4305cc62d42f06808600dafaaa6d2195 introduced a bug in
StillWater because vertices were not initialized correctly. Fix and
remove all remaining references to `localpos` (unused) variables.

src/InputProblem.cc
@@ -244,7 +244,6 @@ void InputProblem::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, v
 {
 	const char *ch_inputfile = inputfile.c_str();
 	uint npart = HDF5SphReader::getNParts(ch_inputfile);
-	float4 localpos;
 
 	HDF5SphReader::ReadParticles *buf = new HDF5SphReader::ReadParticles[npart];
 	HDF5SphReader::readParticles(buf, ch_inputfile, npart);

src/StillWater.cc
@@ -199,19 +199,16 @@ void StillWater::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, has
 
 void StillWater::copy_to_array(float4 *pos, float4 *vel, particleinfo *info, vertexinfo *vertices, float4 *boundelm, hashKey* hash)
 {
-	float4 localpos;
-
 	copy_to_array(pos, vel, info, hash);
 
 	uint j = parts.size() + boundary_parts.size();
 
 	std::cout << "Vertex parts: " << vertex_parts.size() << "\n";
 	for (uint i = j; i < j + vertex_parts.size(); i++) {
-		pos[i] = localpos;
 		float rho = density(H - pos[i].z, 0);
 		vel[i] = make_float4(0, 0, 0, rho);
 		info[i] = make_particleinfo(VERTEXPART, 0, i);
-		calc_localpos_and_hash(vertex_parts[i-j], info[i], localpos, hash[i]);
+		calc_localpos_and_hash(vertex_parts[i-j], info[i], pos[i], hash[i]);
 	}
 	j += vertex_parts.size();
 	std::cout << "Vertex part mass: " << pos[j-1].w << "\n";