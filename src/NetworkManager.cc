GPUSPH/gpusph

commit a5140fb18588e05d3a2ba5a38422811ac4de1a5b

Exchange Buffers over network using new type-agnostic sendBuffer

src/NetworkManager.cc
@@ -99,6 +99,38 @@ void NetworkManager::receiveUint(unsigned char src_globalDevIdx, unsigned char d
 		printf("WARNING: MPI_Get_count returned %d (expected 1)\n", actual_count);
 }
 
+void NetworkManager::sendBuffer(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, void *src_data)
+{
+	unsigned int tag = ((unsigned int)src_globalDevIdx << 8) | dst_globalDevIdx;
+
+	int mpi_err = MPI_Send(src_data, count, MPI_BYTE, GlobalData::RANK(dst_globalDevIdx), tag, MPI_COMM_WORLD);
+
+	if (mpi_err != MPI_SUCCESS)
+		printf("WARNING: MPI_Send returned error %d\n", mpi_err);
+}
+
+void NetworkManager::receiveBuffer(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, void *dst_data)
+{
+	unsigned int tag = ((unsigned int)src_globalDevIdx << 8) | dst_globalDevIdx;
+
+	MPI_Status status;
+	int mpi_err = MPI_Recv(dst_data, count, MPI_BYTE, GlobalData::RANK(src_globalDevIdx), tag, MPI_COMM_WORLD, &status);
+
+	if (mpi_err != MPI_SUCCESS)
+		printf("WARNING: MPI_Recv returned error %d\n", mpi_err);
+	int actual_count;
+
+	mpi_err = MPI_Get_count(&status, MPI_UNSIGNED, &actual_count);
+
+	if (mpi_err != MPI_SUCCESS)
+		printf("WARNING: MPI_Get_count returned error %d\n", mpi_err);
+	else
+	if (actual_count != count)
+		printf("WARNING: MPI_Get_count returned %d (bytes), expected %u\n", actual_count, count);
+}
+
+
+#if 0
 void NetworkManager::sendUints(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, uint *src_data)
 {
 	unsigned int tag = ((unsigned int)src_globalDevIdx << 8) | dst_globalDevIdx;
@@ -188,6 +220,7 @@ void NetworkManager::receiveShorts(unsigned char src_globalDevIdx, unsigned char
 	if (actual_count != count)
 		printf("WARNING: MPI_Get_count returned %d (shorts), expected %u\n", actual_count, count);
 }
+#endif
 
 void NetworkManager::networkFloatReduction(float *buffer, unsigned int bufferElements, ReductionType rtype)
 {

src/NetworkManager.h
@@ -34,12 +34,16 @@ public:
 	// methods to exchange data
 	void sendUint(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int *datum);
 	void receiveUint(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int *datum);
+	void sendBuffer(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, void *src_data);
+	void receiveBuffer(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, void *src_data);
+#if 0
 	void sendUints(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, unsigned int *src_data);
 	void receiveUints(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, unsigned int *dst_data);
 	void sendFloats(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, float *src_data);
 	void receiveFloats(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, float *dst_data);
 	void sendShorts(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, unsigned short *src_data);
 	void receiveShorts(unsigned char src_globalDevIdx, unsigned char dst_globalDevIdx, unsigned int count, unsigned short *dst_data);
+#endif
 	// network reduction on float buffer across the network
 	void networkFloatReduction(float *buffer, unsigned int bufferElements, ReductionType rtype);
 	// send one int, gather the int from all nodes (allgather)

commit e7463dbbb81b023e6c928c89d950fb716b7974fa

Fixed bug introduced in a5140fb18
The size of the received buffer should be measured with respect to the
same data size that was requested

src/NetworkManager.cc
@@ -120,7 +120,7 @@ void NetworkManager::receiveBuffer(unsigned char src_globalDevIdx, unsigned char
 		printf("WARNING: MPI_Recv returned error %d\n", mpi_err);
 	int actual_count;
 
-	mpi_err = MPI_Get_count(&status, MPI_UNSIGNED, &actual_count);
+	mpi_err = MPI_Get_count(&status, MPI_BYTE, &actual_count);
 
 	if (mpi_err != MPI_SUCCESS)
 		printf("WARNING: MPI_Get_count returned error %d\n", mpi_err);