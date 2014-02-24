f3920a572a81af49e42c9aae5d6c141438746f00
Eugenio Rustico <eugenio.rustico@baw.de>
2014-02-24 09:44:37 +0100

    Defined IMPORT_BUFFERS, aka buffers to be imported with APPEND cmd

--- src/define_buffers.h
+++ src/define_buffers.h
@@ -132,6 +132,9 @@ SET_BUFFER_TRAITS(BUFFER_PRIVATE, float, 1, "Private scalar");
 // all particle-based buffers
 #define ALL_PARTICLE_BUFFERS	(ALL_DEFINED_BUFFERS & ~(BUFFERS_CFL | BUFFERS_CELL | BUFFER_NEIBSLIST))
 
+// particle-based buffers to be imported during the APPEND_EXTERNAL command
+#define IMPORT_BUFFERS 			(BUFFER_POS | BUFFER_HASH | BUFFER_VEL | BUFFER_INFO | DBLBUFFER_READ)
+
 // all double buffers TODO some template metaprogramming would help here
 #define BUFFERS_ALL_DBL		(BUFFER_POS | BUFFER_VEL | BUFFER_INFO | \
 	BUFFER_BOUNDELEMENTS | BUFFER_GRADGAMMA | BUFFER_VERTICES | \
