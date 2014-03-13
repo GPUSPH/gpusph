GPUSPH/gpusph

commit 1c3593aa95b913910bd46cb48bc2e139b8a2864a

Do not allocate on device more than whole simulation particles

src/GPUWorker.cc
@@ -206,7 +206,14 @@ void GPUWorker::computeAndSetAllocableParticles()
 
 	freeMemory -= memPerCells;
 
-	m_numAllocatedParticles = (freeMemory / computeMemoryPerParticle());
+	uint numAllocableParticles = (freeMemory / computeMemoryPerParticle());
+
+	if (numAllocableParticles < gdata->allocatedParticles)
+		printf("NOTE: device %u can allocate %u particles, while the whole simulation might require %u\n",
+		       numAllocableParticles, gdata->allocatedParticles);
+
+	// allocate at most the number of particles required for the whole simulation
+	m_numAllocatedParticles = min( numAllocableParticles, gdata->allocatedParticles );
 
 	if (m_numAllocatedParticles < m_numParticles) {
 		fprintf(stderr, "FATAL: thread %u needs %u particles, but we can only store %u in %s available of %s total with %s safety margin\n",

commit e88989ffa9876639dcd047f9b13a9fb0d8422a06

Fixed bug introduced in 1c3593aa
Commit 1c3593aa was cherry-picked from the inoutlet branch, where the
number of allocated particles on host might be higher than totParticles
because of the inlets. However, apart from the inoutlet branch there is
no need for allocatedParticles on host. computeAndSetAllocableParticles()
can safely use GlobalData::totParticles instead.

src/GPUWorker.cc
@@ -208,12 +208,12 @@ void GPUWorker::computeAndSetAllocableParticles()
 
 	uint numAllocableParticles = (freeMemory / computeMemoryPerParticle());
 
-	if (numAllocableParticles < gdata->allocatedParticles)
+	if (numAllocableParticles < gdata->totParticles)
 		printf("NOTE: device %u can allocate %u particles, while the whole simulation might require %u\n",
-			m_deviceIndex, numAllocableParticles, gdata->allocatedParticles);
+			m_deviceIndex, numAllocableParticles, gdata->totParticles);
 
 	// allocate at most the number of particles required for the whole simulation
-	m_numAllocatedParticles = min( numAllocableParticles, gdata->allocatedParticles );
+	m_numAllocatedParticles = min( numAllocableParticles, gdata->totParticles );
 
 	if (m_numAllocatedParticles < m_numParticles) {
 		fprintf(stderr, "FATAL: thread %u needs %u particles, but we can only store %u in %s available of %s total with %s safety margin\n",