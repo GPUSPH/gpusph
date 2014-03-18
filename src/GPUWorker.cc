GPUSPH/gpusph

commit 6f84f90ca3c056b5cd34d08b6523b1dbf268babd

Filling the vertPos buffer when the neighbour list is constructed.

src/GPUWorker.cc
@@ -1859,6 +1859,9 @@ void GPUWorker::kernel_buildNeibsList()
 	buildNeibsList(	m_dBuffers.getData<BUFFER_NEIBSLIST>(),
 					m_dBuffers.getData<BUFFER_POS>(gdata->currentRead[BUFFER_POS]),
 					m_dBuffers.getData<BUFFER_INFO>(gdata->currentRead[BUFFER_INFO]),
+					m_dBuffers.getData<BUFFER_VERTICES>(gdata->currentRead[BUFFER_VERTICES]),
+					m_dBuffers.getData<BUFFER_BOUNDELEMENTS>(gdata->currentRead[BUFFER_BOUNDELEMENTS]),
+					m_dBuffers.get<BUFFER_VERTPOS>()->get_raw_ptr(),
 					m_dBuffers.getData<BUFFER_HASH>(),
 					m_dCellStart,
 					m_dCellEnd,

src/buildneibs.cu
@@ -281,6 +281,9 @@ void
 buildNeibsList(	neibdata*			neibsList,
 				const float4*		pos,
 				const particleinfo*	info,
+				vertexinfo*			vertices,
+				const float4		*boundelem,
+				float2*				vertPos[],
 				const hashKey*		particleHash,
 				const uint*			cellStart,
 				const uint*			cellEnd,
@@ -300,14 +303,22 @@ buildNeibsList(	neibdata*			neibsList,
 	CUDA_SAFE_CALL(cudaBindTexture(0, infoTex, info, numParticles*sizeof(particleinfo)));
 	CUDA_SAFE_CALL(cudaBindTexture(0, cellStartTex, cellStart, gridCells*sizeof(uint)));
 	CUDA_SAFE_CALL(cudaBindTexture(0, cellEndTex, cellEnd, gridCells*sizeof(uint)));
+	CUDA_SAFE_CALL(cudaBindTexture(0, vertTex, vertices, numParticles*sizeof(vertexinfo)));
+	CUDA_SAFE_CALL(cudaBindTexture(0, boundTex, boundelem, numParticles*sizeof(float4)));
 
 	switch (periodicbound) {
 		case 0:
 			cuneibs::buildNeibsListDevice<0, true><<< numBlocks, numThreads >>>(
-					#if (__COMPUTE__ >= 20)
-					pos,
-					#endif
-					particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						#if (__COMPUTE__ >= 20)
+						pos,
+						#endif
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 		break;
 
 		case 1:
@@ -315,7 +326,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 
 		case 2:
@@ -323,7 +340,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 
 		case 3:
@@ -331,7 +354,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 
 		case 4:
@@ -339,7 +368,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 
 		case 5:
@@ -347,7 +382,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 
 		case 6:
@@ -355,7 +396,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 
 		case 7:
@@ -363,7 +410,13 @@ buildNeibsList(	neibdata*			neibsList,
 						#if (__COMPUTE__ >= 20)
 						pos,
 						#endif
-						particleHash,neibsList, particleRangeEnd, sqinfluenceradius);
+						vertPos[0],
+						vertPos[1],
+						vertPos[2],
+						particleHash,
+						neibsList,
+						particleRangeEnd,
+						sqinfluenceradius);
 				break;
 	}
 
@@ -376,6 +429,8 @@ buildNeibsList(	neibdata*			neibsList,
 	CUDA_SAFE_CALL(cudaUnbindTexture(infoTex));
 	CUDA_SAFE_CALL(cudaUnbindTexture(cellStartTex));
 	CUDA_SAFE_CALL(cudaUnbindTexture(cellEndTex));
+	CUDA_SAFE_CALL(cudaUnbindTexture(vertTex));
+	CUDA_SAFE_CALL(cudaUnbindTexture(boundTex));
 }
 
 void

src/buildneibs.cuh
@@ -123,6 +123,9 @@ void
 buildNeibsList(	neibdata*			neibsList,
 				const float4*		pos,
 				const particleinfo*	info,
+				vertexinfo*			vertices,
+				const float4		*boundelem,
+				float2*				vertPos[],
 				const hashKey*		particleHash,
 				const uint*			cellStart,
 				const uint*			cellEnd,

src/buildneibs_kernel.cu
@@ -495,6 +495,9 @@ neibsInCell(
 			#if (__COMPUTE__ >= 20)
 			const float4*	posArray,	///< particle's positions (in)
 			#endif
+			float2*			vertPos0,	///< relative position of vertex to segment, first vertex
+			float2*			vertPos1,	///< relative position of vertex to segment, second vertex
+			float2*			vertPos2,	///< relative position of vertex to segment, third vertex
 			int3			gridPos,	///< current particle grid position
 			const int3		gridOffset,	///< cell offset from current particle grid position
 			const uchar		cell,		///< cell number (0 ... 26)
@@ -503,7 +506,8 @@ neibsInCell(
 			const uint		numParticles,	///< total number of particles
 			const float		sqinfluenceradius,	///< squared value of influence radius
 			neibdata*		neibsList,	///< neighbor's list (out)
-			uint&			neibs_num)	///< number of neighbors for the current particle
+			uint&			neibs_num,	///< number of neighbors for the current particle
+			const bool		segment)	///< if a segment is searching we are only looking for the three vertices
 {
 	// Compute the grid position of the current cell, and return if it's
 	// outside the domain
@@ -523,6 +527,28 @@ neibsInCell(
 	// Substract gridOffset*cellsize to pos so we don't need to do it each time
 	// we compute relPos respect to potential neighbor
 	pos -= gridOffset*d_cellSize;
+	
+	// get vertex indices
+	vertexinfo vertices = make_vertexinfo(0, 0, 0, 0);
+	float4 boundElement = make_float4(0.0f);
+	uint j = 0;
+	float4 coord2 = make_float4(0.0f);
+	if (segment){
+		vertices = tex1Dfetch(vertTex, index);
+		boundElement = tex1Dfetch(boundTex, index);
+		// Get index j for which n_s is minimal
+		if (fabs(boundElement.x) > fabs(boundElement.y))
+			j = 1;
+		if (((float)1-j)*fabs(boundElement.x) + ((float)j)*fabs(boundElement.y) > fabs(boundElement.z))
+			j = 2;
+		// compute second coordinate which is equal to n_s x e_j
+		if (j==0)
+			coord2 = make_float4(0.0f, boundElement.z, -boundElement.y, 0.0f);
+		else if (j==1)
+			coord2 = make_float4(-boundElement.z, 0.0f, boundElement.x, 0.0f);
+		else
+			coord2 = make_float4(boundElement.y, -boundElement.x, 0.0f, 0.0f);
+	}
 
 	// Get the last particle index of the cell
 	const uint bucketEnd = tex1Dfetch(cellEndTex, gridHash);
@@ -530,9 +556,11 @@ neibsInCell(
 	bool encode_cell = true;
 	for(uint neib_index = bucketStart; neib_index < bucketEnd; neib_index++) {
 
-		// Test points are not considered in neighboring list of other particles since they are imaginary particles.
 		const particleinfo info = tex1Dfetch(infoTex, neib_index);
-		if (!TESTPOINTS (info)) {
+		// Test points are not considered in neighboring list of other particles since they are imaginary particles.
+		// If we are looking for neighbours of the boundary segments we only consider vertex particles. For the segments
+		// we actually don't want to fill up the neighbour list, but instead update the vertexPos array.
+		if (!TESTPOINTS (info) || (segment && VERTEX(info))) {
 			// Check for self interaction
 			if (neib_index != index) {
 				// Compute relative position between particle and potential neighbor
@@ -551,13 +579,40 @@ neibsInCell(
 
 				// Check if the squared distance is smaller than the squared influence radius
 				// used for neighbor list construction
-				if (sqlength(relPos) < sqinfluenceradius) {
+				if (sqlength(relPos) < sqinfluenceradius && !segment) {
 					if (neibs_num < d_maxneibsnum) {
 						neibsList[neibs_num*d_neiblist_stride + index] =
 								neib_index - bucketStart + ((encode_cell) ? ENCODE_CELL(cell) : 0);
 						encode_cell = false;
 					}
 					neibs_num++;
+				}
+				else if (segment) {
+					int i = -1;
+					if (neib_index == vertices.x)
+						i = 0;
+					else if (neib_index == vertices.y)
+						i = 1;
+					else if (neib_index == vertices.z)
+						i = 2;
+					if (i>-1) {
+						// relPosProj is the projected relative position of the vertex to the segment.
+						// the first coordinate system is given by the following two vectors:
+						// 1. The unit vector e_j, where j is the coordinate for which n_s is minimal
+						// 2. The cross product between n_s and e_j
+						float2 relPosProj = make_float2(0.0);
+						// relPosProj.x = relPos . e_j
+						relPosProj.x = j==0 ? relPos.x : (j==1 ? relPos.y : relPos.z);
+						// relPosProj.y = relPos . (n_s x e_j)
+						relPosProj.y = dot(relPos, as_float3(coord2));
+						// save relPosProj in vertPos buffer
+						if (i==0)
+							vertPos0[index] = relPosProj;
+						else if (i==1)
+							vertPos1[index] = relPosProj;
+						else
+							vertPos2[index] = relPosProj;
+					}
 				}
 
 			}
@@ -593,6 +648,9 @@ buildNeibsListDevice(
 						#if (__COMPUTE__ >= 20)
 						const float4*	posArray,				///< particle's positions (in)
 						#endif
+						float2*			vertPos0,				///< relative position of vertex to segment, first vertex
+						float2*			vertPos1,				///< relative position of vertex to segment, second vertex
+						float2*			vertPos2,				///< relative position of vertex to segment, third vertex
 						const hashKey*	particleHash,			///< particle's hashes (in)
 						neibdata*		neibsList,				///< neighbor's list (out)
 						const uint		numParticles,			///< total number of particles
@@ -613,7 +671,7 @@ buildNeibsListDevice(
 		// where vertex particles also need to have a list of neighbours
 
 		// Neighbor list is build for fluid, object, vertex and test particles
-		if (FLUID(info) || TESTPOINTS (info) || OBJECT(info) || VERTEX(info) /*TODO: || BOUNDARY(info)*/) {
+		if (FLUID(info) || TESTPOINTS (info) || OBJECT(info) || VERTEX(info) || BOUNDARY(info)) {
 			// Get particle position
 			#if (__COMPUTE__ >= 20)
 			const float4 pos = posArray[index];
@@ -636,8 +694,18 @@ buildNeibsListDevice(
 								#if (__COMPUTE__ >= 20)
 								posArray,
 								#endif
-								gridPos, make_int3(x, y, z), (x + 1) + (y + 1)*3 + (z + 1)*9, index, pos3,
-								numParticles, sqinfluenceradius, neibsList, neibs_num);
+								vertPos0,
+								vertPos1,
+								vertPos2,
+								gridPos,
+								make_int3(x, y, z), (x + 1) + (y + 1)*3 + (z + 1)*9,
+								index,
+								pos3,
+								numParticles,
+								sqinfluenceradius,
+								neibsList,
+								neibs_num,
+								BOUNDARY(info));
 						}
 					}
 				}

commit 1972816d09d0e8ce156eda0c84af0c70952f2154

Prevent get_raw_ptr() from segfaulting
Using BufferList::get<Key>()->get_raw_ptr() would lead to a segfault if
the specified Key buffer was not allocated. It's ok to get a NULL
pointer in this case, so wrap the whole thing into a new BufferList
method that does it for us. get_raw_ptr() now becomes protected because
we don't want to access it directly in user code anyway.

Issue introduced by 6f84f90ca3c056b5cd34d08b6523b1dbf268babd in problems
that don't use SA boundary conditions.

src/GPUWorker.cc
@@ -1844,7 +1844,7 @@ void GPUWorker::kernel_buildNeibsList()
 					m_dBuffers.getData<BUFFER_INFO>(gdata->currentRead[BUFFER_INFO]),
 					m_dBuffers.getData<BUFFER_VERTICES>(gdata->currentRead[BUFFER_VERTICES]),
 					m_dBuffers.getData<BUFFER_BOUNDELEMENTS>(gdata->currentRead[BUFFER_BOUNDELEMENTS]),
-					m_dBuffers.get<BUFFER_VERTPOS>()->get_raw_ptr(),
+					m_dBuffers.getRawPtr<BUFFER_VERTPOS>(),
 					m_dBuffers.getData<BUFFER_HASH>(),
 					m_dCellStart,
 					m_dCellEnd,
@@ -1880,7 +1880,7 @@ void GPUWorker::kernel_forces()
 	if (numPartsToElaborate > 0 )
 		returned_dt = forces(
 						m_dBuffers.getData<BUFFER_POS>(gdata->currentRead[BUFFER_POS]),   // pos(n)
-						m_dBuffers.get<BUFFER_VERTPOS>()->get_raw_ptr(),
+						m_dBuffers.getRawPtr<BUFFER_VERTPOS>(),
 						m_dBuffers.getData<BUFFER_VEL>(gdata->currentRead[BUFFER_VEL]),   // vel(n)
 						m_dBuffers.getData<BUFFER_FORCES>(),					// f(n
 						m_dBuffers.getData<BUFFER_GRADGAMMA>(gdata->currentRead[BUFFER_GRADGAMMA]),
@@ -2058,7 +2058,7 @@ void GPUWorker::kernel_sps()
 	// is the device empty? (unlikely but possible before LB kicks in)
 	if (numPartsToElaborate == 0) return;
 
-	sps(m_dBuffers.get<BUFFER_TAU>()->get_raw_ptr(),
+	sps(m_dBuffers.getRawPtr<BUFFER_TAU>(),
 		m_dBuffers.getData<BUFFER_POS>(gdata->currentRead[BUFFER_POS]),
 		m_dBuffers.getData<BUFFER_VEL>(gdata->currentRead[BUFFER_VEL]),
 		m_dBuffers.getData<BUFFER_INFO>(gdata->currentRead[BUFFER_INFO]),

src/buffer.h
@@ -114,6 +114,13 @@ class GenericBuffer : public AbstractBuffer
 protected:
 	enum { array_count = N };
 
+	// accessors to the raw pointers. used by specializations
+	// to easily handle allocs and frees
+	virtual T** get_raw_ptr()
+	{ return m_bufs; }
+	virtual const T* const* get_raw_ptr() const
+	{ return m_bufs; }
+
 public:
 	typedef T element_type;
 
@@ -125,14 +132,6 @@ public:
 	}
 
 	virtual ~GenericBuffer() {} ;
-
-	// accessors to the raw pointers. used by specializations
-	// to easily handle allocs and frees, but must be public
-	// because it's also used in GPUSPH proper for the TAU buffer
-	virtual T** get_raw_ptr()
-	{ return m_bufs; }
-	virtual const T* const* get_raw_ptr() const
-	{ return m_bufs; }
 
 	// return an (untyped) pointer to the idx buffer,
 	// if valid. Must return void since we overload
@@ -179,6 +178,10 @@ public:
  * we just add a method to get the printable name associated with the given Key.
  */
 
+// Forward-declare the BufferList class, which we want to be friend with the
+// Buffer classes so that it may access the protected get_raw_ptr() methods.
+class BufferList;
+
 // since some people find this particular aspect of the C++ syntax a bit too ugly,
 // let's introduce a de-uglifying macro that just returns the expected data type for
 // elements in array Key (e.g. float4 for BUFFER_POS):
@@ -190,6 +193,7 @@ class Buffer : public GenericBuffer<DATA_TYPE(Key), BufferTraits<Key>::num_buffe
 	// we want to use baseclass as a shortcut for the parent class of Buffer<Key>
 	typedef GenericBuffer<DATA_TYPE(Key), BufferTraits<Key>::num_buffers> baseclass;
 
+	friend class BufferList;
 public:
 
 	// constructor, specifying the memset initializer
@@ -308,6 +312,28 @@ public:
 		const_iterator exists = this->find(Key);
 		if (exists != this->end())
 			return static_cast<const DATA_TYPE(Key)*>(exists->second->get_buffer(num));
+		else return NULL;
+	}
+
+	/* In a few cases, the user may want to access the base pointer to
+	 * multi-buffer arrays. This could be done using get<Key>()->get_raw_ptr,
+	 * but this causes a segmentation fault when the Key array isn't actually
+	 * allocated, while we really want to return NULL in this case. Wrap it up
+	 * for safety.
+	 */
+	template<flag_t Key>
+	DATA_TYPE(Key) **getRawPtr() {
+		Buffer<Key> *exists = this->get<Key>();
+		if (exists)
+			return exists->get_raw_ptr();
+		else return NULL;
+	}
+	// const version
+	template<flag_t Key>
+	const DATA_TYPE(Key)* const* getRawPtr() const {
+		Buffer<Key> *exists = this->get<Key>();
+		if (exists)
+			return exists->get_raw_ptr();
 		else return NULL;
 	}
 