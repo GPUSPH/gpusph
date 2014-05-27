06abb5cab2fcb43fb5fd7fa7bea03ab7b83c3c15
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2014-05-27 15:34:39 +0200

    Fix warnings in NAN reporting for doWrite

--- src/GPUSPH.cc
+++ src/GPUSPH.cc
@@ -1131,8 +1131,8 @@ void GPUSPH::doWrite(bool force)
 		dpos.w = pos.w;
 
 		if (!warned_nan_pos && !(isfinite(dpos.x) && isfinite(dpos.y) && isfinite(dpos.z))) {
-			fprintf(stderr, "WARNING: particle %u (%u) has NAN position! (%g, %g, %g) @ (%g, %g, %g) = (%g, %g, %g)\n",
-				i, info[i],
+			fprintf(stderr, "WARNING: particle %u (%u) has NAN position! (%g, %g, %g) @ (%u, %u, %u) = (%g, %g, %g)\n",
+				i, id(info[i]),
 				pos.x, pos.y, pos.z,
 				gridPos.x, gridPos.y, gridPos.z,
 				dpos.x, dpos.y, dpos.z);
