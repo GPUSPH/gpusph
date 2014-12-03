19dc2067f3efd396aeda25f8d2dfc39dc08b615c
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2014-12-03 23:34:00 +0100

    Restore XZY linearization
    This reverts commit 4ceb408c2679f718bb6e77f0d5a605934659c098.

--- src/linearization.h
+++ src/linearization.h
@@ -6,6 +6,6 @@
  * thus eligible for a single burst transfer.
  * Cells with consecutive COORD1 are consecutive in their linearized index. */
 
-#define COORD1	y
+#define COORD1	x
 #define COORD2	z
-#define COORD3	x
+#define COORD3	y
