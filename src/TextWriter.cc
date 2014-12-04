a6a05520cb868392fa7b9a7ac68ac7cd791fdd6e
Arno Mayrhofer <arno.m@gmx.at>
2014-12-04 14:36:19 +0100

    Fix pressure output in other writers

--- src/TextWriter.cc
+++ src/TextWriter.cc
@@ -81,7 +81,7 @@ TextWriter::write(uint numParts, BufferList const& buffers, uint node_offset, do
 
 		// pressure
 		if (FLUID(info[i]))
-			fid << m_problem->pressure(vel[i].w, object(info[i])) << "\t";
+			fid << m_problem->pressure(vel[i].w, PART_FLUID_NUM(info[i])) << "\t";
 		else if (TESTPOINTS(info[i]))
 			fid << vel[i].w << "\t";
 		else
