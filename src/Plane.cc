cc58022f1f39ce94dedc4bdffa1cfc7babc5883b
Eugenio Rustico <narcolesssico@gmail.com>
2014-12-04 16:58:09 +0100

    Implement Plane class deriving from Object

---
+++ src/Plane.cc
@@ -0,0 +1,78 @@
+/*  Copyright 2011-2013 Alexis Herault, Giuseppe Bilotta, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+
+    Istituto Nazionale di Geofisica e Vulcanologia
+        Sezione di Catania, Catania, Italy
+
+    Università di Catania, Catania, Italy
+
+    Johns Hopkins University, Baltimore, MD
+
+    This file is part of GPUSPH.
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Plane.h"
+
+Plane::Plane(const double a, const double b, const double c, const double d)
+{
+	m_a = a;
+	m_b = b;
+	m_c = c;
+	m_d = d;
+	m_norm = sqrt(m_a * m_a + m_b * m_b + m_c * m_c);
+}
+
+void Plane::SetInertia(const double dx)
+{
+	throw std::runtime_error("Trying to set inertia on a plane!");
+}
+
+void Plane::FillBorder(PointVect& points, const double dx)
+{
+	printf("WARNING: FillBorder not implemented for planes!\n");
+}
+
+int Plane::Fill(PointVect& points, const double dx, const bool fill)
+{
+	printf("WARNING: Fill not implemented for planes!\n");
+	return 0;
+}
+
+void Plane::FillIn(PointVect& points, const double dx, const int layers)
+{
+	printf("WARNING: FillIn not implemented for planes!\n");
+}
+
+bool Plane::IsInside(const Point& p, const double dx)
+{
+	const double distance = (m_a * p(0) + m_b * p(1) + m_c * p(2) + m_d) / m_norm;
+	return distance > -dx;
+}
+
+void Plane::setEulerParameters(const EulerParameters &ep)
+{
+	throw std::runtime_error("Trying to set EulerParameters on a plane!");
+}
+
+void Plane::ODEBodyCreate(dWorldID ODEWorld, const double dx, dSpaceID ODESpace)
+{
+	throw std::runtime_error("Trying to instantiate an ODEBody for a plane!");
+}
+
+void Plane::ODEGeomCreate(dSpaceID ODESpace, const double dx)
+{
+	// NOTE: inverting m_d because ODE uses a+b+c=d
+	m_ODEGeom = dCreatePlane(ODESpace, m_a, m_b, m_c, -m_d);
+}
---
+++ src/Plane.h
@@ -0,0 +1,67 @@
+/*  Copyright 2011-2013 Alexis Herault, Giuseppe Bilotta, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+
+    Istituto Nazionale di Geofisica e Vulcanologia
+        Sezione di Catania, Catania, Italy
+
+    Università di Catania, Catania, Italy
+
+    Johns Hopkins University, Baltimore, MD
+
+    This file is part of GPUSPH.
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _PLANE_H
+#define _PLANE_H
+
+#include "Object.h"
+
+//! Plane object class
+class Plane: public Object {
+	private:
+		double m_a;		// a coefficient of implicit equation
+		double m_b;		// b coefficient of implicit equation
+		double m_c;		// c coefficient of implicit equation
+		double m_d;		// d coefficient of implicit equation
+		double m_norm;	// norm
+
+	public:
+		/// \name Constructors and destructor
+		//@{
+		Plane(const double a, const double b, const double c, const double d);
+		virtual ~Plane() {};
+		//@}
+
+		double Volume(const double dx);
+		void SetInertia(const double dx);
+
+		/// \name Filling functions
+		//@{
+		void FillBorder(PointVect& points, const double dx);
+		int Fill(PointVect& points, const double dx, const bool fill);
+		void FillIn(PointVect& points, const double dx, const int layers);
+
+		bool IsInside(const Point& p, const double dx);
+		void setEulerParameters(const EulerParameters &ep);
+
+		/// \name ODE related  functions
+		//@{
+		void ODEBodyCreate(dWorldID world, const double dx, dSpaceID ODESpace);
+		void ODEGeomCreate(dSpaceID space, const double dx);
+		//@}
+};
+
+#endif /* _PLANE_H */
+
