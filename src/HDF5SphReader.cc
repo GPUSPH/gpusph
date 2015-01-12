b8832ad8e220d8d551150a23cb3c17f15c860149
Eugenio Rustico <eugenio.rustico@baw.de>
2015-01-12 09:37:23 +0100

    HDF5SphReader: load number of particles if getNParts() was not called yet

--- src/HDF5SphReader.cc
+++ src/HDF5SphReader.cc
@@ -66,6 +66,9 @@ void
 HDF5SphReader::read()
 {
 #if USE_HDF5
+	// read npart if it was yet uninitialized
+	if (npart == UINT_MAX)
+		getNParts();
 	std::cout << std::endl << "Reading particle data from the input:" << std::endl << filename << std::endl;
 	if(buf == NULL)
 		buf = new ReadParticles[npart];
