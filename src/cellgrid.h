dc8b2ddf4bc2dda41ff8b4a653e26604a51cc87e
alexisherault <alexis.herault@cnam.fr>
2015-02-26 14:03:12 -0500

    Fixed documentation.

--- src/cellgrid.h
+++ src/cellgrid.h
@@ -1,13 +1,16 @@
-/*  Copyright 2013 Alexis Herault, Giuseppe Bilotta, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+/*  Copyright 2013 Alexis Herault, Giuseppe Bilotta, Robert A.
+ 	Dalrymple, Eugenio Rustico, Ciro Del Negro
 
-    Istituto Nazionale di Geofisica e Vulcanologia
-        Sezione di Catania, Catania, Italy
+	Conservatoire National des Arts et Metiers, Paris, France
 
-    Università di Catania, Catania, Italy
+	Istituto Nazionale di Geofisica e Vulcanologia,
+    Sezione di Catania, Catania, Italy
+
+    Universita di Catania, Catania, Italy
 
     Johns Hopkins University, Baltimore, MD
 
-    This file is part of GPUSPH.
+	This file is part of GPUSPH.
 
     GPUSPH is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -34,20 +37,29 @@
 #define _CELLGRID_H
 
 #include "hashkey.h"
-
-// COORD1, COORD2, COORD3
 #include "linearization.h"
 
 __constant__ float3 d_worldOrigin;
 __constant__ float3 d_cellSize;
 __constant__ uint3 d_gridSize;
 
-// Compute cell hash (linearized index) from cell coordinates. No clamping/periodicity checks are done.
+
+/// Compute hash value from grid position
+/*! Compute the hash value from grid position according to the chosen
+ * 	linearization (starting from x, y or z direction). The link
+ * 	between COORD1,2,3 and .x, .y and .z is defined in linearization.h
+ *
+ *	\param[in] gridPos : grid position
+ *
+ *	\return hash value
+ */
 __device__ __forceinline__ uint
 calcGridHash(int3 const& gridPos)
 {
-	return INTMUL(INTMUL(gridPos.COORD3, d_gridSize.COORD2), d_gridSize.COORD1) + INTMUL(gridPos.COORD2, d_gridSize.COORD1) + gridPos.COORD1;
+	return INTMUL(INTMUL(gridPos.COORD3, d_gridSize.COORD2), d_gridSize.COORD1)
+			+ INTMUL(gridPos.COORD2, d_gridSize.COORD1) + gridPos.COORD1;
 }
+
 
 /// Compute grid position from cell hash value
 /*! Compute the grid position corresponding to the given cell hash. The position
@@ -72,6 +84,7 @@ calcGridPosFromCellHash(const uint cellHash)
 	return gridPos;
 }
 
+
 /// Compute grid position from particle hash value
 /*! Compute the grid position corresponding to the given particle hash. The position
  * 	should be in the range [0, d_gridSize.x - 1]x[0, d_gridSize.y - 1]x[0, d_gridSize.z - 1].
@@ -86,12 +99,13 @@ calcGridPosFromCellHash(const uint cellHash)
 __device__ __forceinline__ int3
 calcGridPosFromParticleHash(const hashKey particleHash)
 {
-	// read the cellHash out of the particleHash
+	// Read the cellHash out of the particleHash
 	const uint cellHash = cellHashFromParticleHash(particleHash);
 	return calcGridPosFromCellHash(cellHash);
 }
 
-/* the neighbor cell num ranges from 1 to 27 (included), so it fits in
+
+/* The neighbor cell num ranges from 1 to 27 (included), so it fits in
  * 5 bits, which we put in the upper 5 bits of the neibdata, which is
  * 16-bit wide.
  * TODO actually compute this from sizeof(neibdata)
@@ -101,7 +115,6 @@ calcGridPosFromParticleHash(const hashKey particleHash)
 #define NEIBINDEX_MASK	(CELLNUM_ENCODED-1)
 #define ENCODE_CELL(cell) ((cell + 1) << CELLNUM_SHIFT)
 #define DECODE_CELL(data) ((data >> CELLNUM_SHIFT) - 1)
-
 
 #endif
 
