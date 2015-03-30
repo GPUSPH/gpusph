de748528fae5f9f8f9e208be2fc5bd3e35f39147
alexisherault <alexis.herault@cnam.fr>
2015-03-30 12:13:18 -0400

    WIP: added ACTIVE(neib_pos) in Shepard neib test.

--- src/cuda/forces_kernel.cu
+++ src/cuda/forces_kernel.cu
@@ -1726,7 +1726,7 @@ shepardDevice(	const float4*	posArray,
 		const particleinfo neib_info = tex1Dfetch(infoTex, neib_index);
 
 		//if (r < influenceradius && (FLUID(neib_info)/* || VERTEX(neib_info)*/) ) {
-		if (r < influenceradius && (FLUID(neib_info) || boundarytype == DYN_BOUNDARY) ) {
+		if (r < influenceradius && ACTIVE(relPos) && (FLUID(neib_info) || boundarytype == DYN_BOUNDARY) ) {
 			const float w = W<kerneltype>(r, slength)*relPos.w;
 			temp1 += w;
 			temp2 += w/neib_rho;
