22d65797a5410b37578dee7a3224e6e2a0eae1cc
alexisherault <alexis.herault@cnam.fr>
2015-03-30 11:51:05 -0400

    WIP: added boundary type for shepard.

--- src/cuda/forces.cu
+++ src/cuda/forces.cu
@@ -736,7 +736,7 @@ struct CUDAFilterEngineHelper<SHEPARD_FILTER, kerneltype, boundarytype>
 	dummy_shared = 2560;
 	#endif
 
-	cuforces::shepardDevice<kerneltype><<< numBlocks, numThreads, dummy_shared >>>
+	cuforces::shepardDevice<kerneltype, boundarytype><<< numBlocks, numThreads, dummy_shared >>>
 		(pos, newVel, particleHash, cellStart, neibsList, particleRangeEnd, slength, influenceradius);
 
 	// check if kernel invocation generated an error
