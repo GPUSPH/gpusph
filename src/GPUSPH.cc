GPUSPH/gpusph

commit e9948913fa4af30601c25ca9374a72c9bacb4cc5

GPUSPH: track peak particle speed on save, print at the end who and when
Useful for non-conservative setting  of sspeed.

NOTE: assuming velocity is dumped (currently in which_buffers)

src/GPUSPH.cc
@@ -64,6 +64,8 @@ GPUSPH::GPUSPH() {
 	gdata = NULL;
 	problem = NULL;
 	initialized = false;
+	m_peakParticleSpeed = 0.0;
+	m_peakParticleSpeedTime = 0.0;
 }
 
 GPUSPH::~GPUSPH() {
@@ -833,6 +835,10 @@ bool GPUSPH::runSimulation() {
 	if (MULTI_NODE)
 		printf("Global performance of the multinode simulation: %.2g MIPPS\n", m_multiNodePerformanceCounter->getMIPPS());
 
+	// suggest max speed for next runs
+	printf("Peak particle speed was ~%g at t %g -> can set maximum vel %.2g for this problem\n",
+		m_peakParticleSpeed, m_peakParticleSpeedTime, (m_peakParticleSpeed*1.1));
+
 	// NO doCommand() nor other barriers than the standard ones after the
 
 	printf("Simulation end, cleaning up...\n");
@@ -1320,6 +1326,16 @@ void GPUSPH::doWrite(bool force)
 		}
 
 		gpos[i] = dpos;
+
+		// track peak speed
+		float part_speed = length( as_float3( gdata->s_hBuffers.getData<BUFFER_VEL>()[i] ) );
+		// read simulation global for multi-node
+		if (MULTI_NODE)
+			gdata->networkManager->networkFloatReduction(&(part_speed), 1, MAX_REDUCTION);
+		if (part_speed > m_peakParticleSpeed) {
+			m_peakParticleSpeed = part_speed;
+			m_peakParticleSpeedTime = gdata->t;
+		}
 	}
 
 	Writer::SetForced(force);

src/GPUSPH.h
@@ -56,6 +56,11 @@ private:
 	bool *m_rcNotified;
 	uint *m_rcAddrs;
 
+	// store max speed reached during the whole simulation
+	// NOTE: float since network reduction currently does not support double
+	float m_peakParticleSpeed;
+	double m_peakParticleSpeedTime; // ...and when
+
 	// other vars
 	bool initialized;
 

commit 5d7cd7544028d1e3fdd5b8f1f1008f44a5b6c677

Fix bugs introduced in e9948913
A barrier like a network reduction makes no sense in a local cycle.

Also, time of peack speed was not being updated.

src/GPUSPH.cc
@@ -1295,6 +1295,9 @@ void GPUSPH::doWrite(bool force)
 
 	bool warned_nan_pos = false;
 
+	// max particle speed only for this node only at time t
+	float local_max_part_speed = 0;
+
 	for (uint i = node_offset; i < node_offset + gdata->processParticles[gdata->mpi_rank]; i++) {
 		const float4 pos = lpos[i];
 		double4 dpos;
@@ -1328,14 +1331,17 @@ void GPUSPH::doWrite(bool force)
 		gpos[i] = dpos;
 
 		// track peak speed
-		float part_speed = length( as_float3( gdata->s_hBuffers.getData<BUFFER_VEL>()[i] ) );
-		// read simulation global for multi-node
-		if (MULTI_NODE)
-			gdata->networkManager->networkFloatReduction(&(part_speed), 1, MAX_REDUCTION);
-		if (part_speed > m_peakParticleSpeed) {
-			m_peakParticleSpeed = part_speed;
-			m_peakParticleSpeedTime = gdata->t;
-		}
+		local_max_part_speed = fmax(local_max_part_speed, length( as_float3(gdata->s_hBuffers.getData<BUFFER_VEL>()[i]) ));
+	}
+
+	// max speed: read simulation global for multi-node
+	if (MULTI_NODE)
+		// after this, local_max_part_speed actually becomes global_max_part_speed for time t only
+		gdata->networkManager->networkFloatReduction(&(local_max_part_speed), 1, MAX_REDUCTION);
+	// update peak
+	if (local_max_part_speed > m_peakParticleSpeed) {
+		m_peakParticleSpeed = local_max_part_speed;
+		m_peakParticleSpeedTime = gdata->t;
 	}
 
 	Writer::SetForced(force);