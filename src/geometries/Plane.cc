408694c51fa7f136bfedbe5e01818c9f09d40c18
Eugenio Rustico <eugenio.rustico@baw.de>
2015-10-06 13:35:51 +0200

    Minor typo

--- src/geometries/Plane.cc
+++ src/geometries/Plane.cc
@@ -89,7 +89,7 @@ void Plane::getBoundingBox(Point &output_min, Point &output_max)
 void Plane::shift(const double3 &offset)
 {
 	const Point poff = Point(offset);
-	// also update center altough it has little meaning for a plane
+	// also update center although it has little meaning for a plane
 	m_center += poff;
 	printf("m_d was %g, off %g %g %g\n", m_d, offset.x, offset.y, offset.z);
 	m_d += m_a * offset.x + m_b * offset.y + m_c * offset.z;
