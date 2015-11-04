8188aad89513e006d9fcdbbab0a2acbf0a0d2391
alexisherault <alexis.herault@cnam.fr>
2015-11-04 16:52:20 +0100

    Implemented partial specilaization used for boundary-fluid and fluid-boundary interaction with LJ and MK. Tested with LJ on DamBreak3D.

--- src/problems/DamBreak3D.cu
+++ src/problems/DamBreak3D.cu
@@ -65,7 +65,7 @@ DamBreak3D::DamBreak3D(GlobalData *_gdata) : Problem(_gdata)
 
 	SETUP_FRAMEWORK(
 		viscosity<ARTVISC>,
-		boundary<DYN_BOUNDARY>
+		boundary<LJ_BOUNDARY>
 	).select_options(
 		rhodiff, FlagSwitch<ENABLE_NONE, ENABLE_DENSITY_DIFFUSION, ENABLE_FERRARI>(),
 		m_usePlanes, add_flags<ENABLE_PLANES>()
