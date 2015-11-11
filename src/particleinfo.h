GPUSPH/gpusph

commit c5b42974d30c4aac0932a19a428b71b08bbc854c

Fix compilation bug for non dtadapt and density_sum

src/cuda/forces_kernel.def
@@ -2023,7 +2023,7 @@ struct dyndt_keps_shared_data
 template<BoundaryType boundarytype, ViscosityType visctype, flag_t simflags>
 struct forces_shared_data :
 	COND_SHARED(simflags & ENABLE_DTADAPT, dyndt_shared_data),
-	COND_SHARED(simflags & (ENABLE_DTADAPT | ENABLE_DENSITY_SUM) &&
+	COND_SHARED(simflags & (ENABLE_DTADAPT & ENABLE_DENSITY_SUM) &&
 		boundarytype == SA_BOUNDARY, dyndt_densitySum_shared_data),
 	COND_SHARED(simflags & ENABLE_DTADAPT &&
 		visctype == KEPSVISC, dyndt_keps_shared_data)
@@ -2032,7 +2032,7 @@ struct forces_shared_data :
 		typename COND_SHARED(simflags & ENABLE_DTADAPT, dyndt_shared_data)
 		common;
 	typedef
-		typename COND_SHARED(simflags & (ENABLE_DTADAPT | ENABLE_DENSITY_SUM) &&
+		typename COND_SHARED(simflags & (ENABLE_DTADAPT & ENABLE_DENSITY_SUM) &&
 			boundarytype == SA_BOUNDARY, dyndt_densitySum_shared_data)
 		density_sum;
 	typedef

commit abdb9f2142d4b102cd877ed973267b4099946fc1

Flag query macros QUERY_{ALL,ANY}_{,PART_}FLAGS
Fixes a bug introduced in c5b42974d30c4aac0932a19a428b71b08bbc854c
Allows for flags to be checked in bit fields, if either any or all of
them are set. The macros containing PART_ operate on a particleinfo
object

src/cuda/forces_kernel.def
@@ -2023,7 +2023,7 @@ struct dyndt_keps_shared_data
 template<BoundaryType boundarytype, ViscosityType visctype, flag_t simflags>
 struct forces_shared_data :
 	COND_SHARED(simflags & ENABLE_DTADAPT, dyndt_shared_data),
-	COND_SHARED(simflags & (ENABLE_DTADAPT & ENABLE_DENSITY_SUM) &&
+	COND_SHARED(QUERY_ALL_FLAGS(simflags, ENABLE_DTADAPT | ENABLE_DENSITY_SUM) &&
 		boundarytype == SA_BOUNDARY, dyndt_densitySum_shared_data),
 	COND_SHARED(simflags & ENABLE_DTADAPT &&
 		visctype == KEPSVISC, dyndt_keps_shared_data)
@@ -2032,7 +2032,7 @@ struct forces_shared_data :
 		typename COND_SHARED(simflags & ENABLE_DTADAPT, dyndt_shared_data)
 		common;
 	typedef
-		typename COND_SHARED(simflags & (ENABLE_DTADAPT & ENABLE_DENSITY_SUM) &&
+		typename COND_SHARED(QUERY_ALL_FLAGS(simflags, ENABLE_DTADAPT & ENABLE_DENSITY_SUM) &&
 			boundarytype == SA_BOUNDARY, dyndt_densitySum_shared_data)
 		density_sum;
 	typedef

src/particleinfo.h
@@ -95,7 +95,10 @@ enum ParticleFlag {
 
 #define SET_FLAG(info, flag) ((info).x |= (flag))
 #define CLEAR_FLAG(info, flag) ((info).x &= ~(flag))
-#define QUERY_FLAG(info, flag) ((info).x & (flag))
+/// Query whether all flags are set in the particleinfo struct of a certain particle
+#define QUERY_ALL_PART_FLAG(info, flags) ((info).x & (flags) == (flags))
+/// Query whether at least one flag (out of flags) is set in the particleinfo struct of a certain particle
+#define QUERY_ANY_PART_FLAG(info, flags) ((info).x & (flags))
 
 /// Bitmasks to select only the particle type or flags
 #define PART_TYPE_MASK	((1<<PART_FLAG_SHIFT)-1)

src/simflags.h
@@ -79,4 +79,9 @@
 // in case LAST_SIMFLAG is already the last bit
 #define ENABLE_ALL_SIMFLAGS		(LAST_SIMFLAG | (LAST_SIMFLAG-1))
 
+/// General query that identifies whether the flags in field are set, true only if all of them are
+#define QUERY_ALL_FLAGS(field, flags)	((field) & (flags) == (flags))
+/// General query that identifies whether at least one flag in field is set
+#define QUERY_ANY_FLAGS(field, flags)	((field) & (flags))
+
 #endif