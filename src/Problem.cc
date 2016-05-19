GPUSPH/gpusph

commit 3dbc20f4a8f4160c09cc3fa205552bf513d62264

Minor changes in several problem files

src/Problem.cc
@@ -69,7 +69,8 @@ Problem::density(float h, int i)
 	float density = m_physparams.rho0[i];
 
 	if (h > 0) {
-		float g = length(m_physparams.gravity);
+		//float g = length(m_physparams.gravity);
+		float g = abs(m_physparams.gravity.z);
 		density = m_physparams.rho0[i]*pow(g*m_physparams.rho0[i]*h/m_physparams.bcoeff[i] + 1,
 				1/m_physparams.gammacoeff[i]);
 		}

src/DamBreak3D.cc
@@ -73,6 +73,7 @@ DamBreak3D::DamBreak3D(const Options &options) : Problem(options)
 	m_simparams.buildneibsfreq = 10;
 	m_simparams.shepardfreq = 0;
 	m_simparams.mlsfreq = 0;
+	m_simparams.ferrari = 0.1;
 	//m_simparams.visctype = ARTVISC;
 	m_simparams.visctype = DYNAMICVISC;
 	m_simparams.boundarytype= LJ_BOUNDARY;

src/InputProblem.cc
@@ -15,27 +15,61 @@
 
 InputProblem::InputProblem(const Options &options) : Problem(options)
 {
-	//inputfile = "/home/vorobyev/Crixus/geometries/spheric2/0.spheric2-dr-0.01833-dp-0.02.h5sph";		//Spheric 2 (DamBreak)
 	//inputfile = "/home/vorobyev/Crixus/geometries/fishpass3D/0.fishpass_pv.h5sph";			//Fishpass 3D (not working yet)
 	//inputfile = "/home/vorobyev/Crixus/geometries/plane_periodicity/0.plane_0.1.h5sph";			//StillWater periodic
-	inputfile = "/home/vorobyev/Crixus/geometries/plane_periodicity/0.plane_0.1_sym.h5sph";		//StillWater periodic (symmetric)
 	//inputfile = "/home/vorobyev/Crixus/geometries/opencube/0.opencube_salome.h5sph";			//StillWater (meshed in Salome)
 	//inputfile = "/home/vorobyev/Crixus/geometries/2planes_periodicity/0.2planes_0.05.h5sph";		//Plane Poiseuille flow
 	numparticles = 0;
+
+	//StillWater periodic (symmetric)
+	//*************************************************************************************
+//	inputfile = "/home/vorobyev/Crixus/geometries/plane_periodicity/0.plane_0.1_sym.h5sph";
+//	n_probeparts = 0;
+//	H = 2.0;
+//	l = 2.0; w = 2.0; h = 2.2;
+
+//	set_deltap(0.1f);
+//
+//	//periodic boundaries
+//	m_simparams.periodicbound = true;
+//	m_physparams.dispvect = make_float3(l, l, 0.0);
+//	m_physparams.minlimit = make_float3(0.0f, 0.0f, 0.0f);
+//	m_physparams.maxlimit = make_float3(l, l, 0.0f);
+	//*************************************************************************************
+
+	//Spheric 2 (DamBreak)
+	//*************************************************************************************
+//	inputfile = "/home/vorobyev/Crixus/geometries/spheric2/0.spheric2-dr-0.01833-dp-0.02.h5sph";
+
+//	set_deltap(0.02f);
+
+//	m_physparams.gravity = make_float3(0.0, 0.0, -9.81f);
+
 //	n_probeparts = 208;
 //	H = 0.55;
 //	l = 3.5; w = 1.0; h = 1.0;
-	n_probeparts = 0;
-	H = 2.0;
-	l = 2.0; w = 2.0; h = 2.2;
+	//*************************************************************************************
 
-	set_deltap(0.1f);
+	// Fishpass
+	//*************************************************************************************
+	inputfile = "/home/vorobyev/Crixus/geometries/fishpass3D/wrong.fishpass_covered_0.0075_sl10.h5sph";
+
+	set_deltap(0.0075f);
+
+	n_probeparts = 0;
+	H = 0.2;
+	l = 0.75; w = 0.675; h = 0.4;
+
+	float slope = 0.1;
+
+	m_physparams.gravity = make_float3(9.81f*sin(atan(slope)), 0.0, -9.81f*cos(atan(slope)));
 
 	//periodic boundaries
 	m_simparams.periodicbound = true;
-	m_physparams.dispvect = make_float3(l, l, 0.0);
+	m_physparams.dispvect = make_float3(l, 0.0f, 0.0f);
 	m_physparams.minlimit = make_float3(0.0f, 0.0f, 0.0f);
-	m_physparams.maxlimit = make_float3(l, l, 0.0f);
+	m_physparams.maxlimit = make_float3(l, 0.0f, 0.0f);
+	//*************************************************************************************
 
 	// SPH parameters
 	m_simparams.slength = 1.3f*m_deltap;
@@ -48,7 +82,7 @@ InputProblem::InputProblem(const Options &options) : Problem(options)
 	m_simparams.buildneibsfreq = 10;
 	m_simparams.shepardfreq = 0;
 	m_simparams.mlsfreq = 0;
-	m_simparams.ferrari = 0.1f;
+	m_simparams.ferrari = 1.0;
 	m_simparams.visctype = DYNAMICVISC;
 	//m_simparams.visctype = KINEMATICVISC;
 	//m_simparams.visctype = ARTVISC;
@@ -62,17 +96,16 @@ InputProblem::InputProblem(const Options &options) : Problem(options)
 	m_writerType = VTKWRITER;
 
 	// Physical parameters
-	m_physparams.gravity = make_float3(0.0, 0.0, -9.81f);
 	//m_physparams.gravity = make_float3(0.8, 0.0, 0.0); //body forse for plane Poiseuille flow
 	float g = length(m_physparams.gravity);
-	m_physparams.set_density(0, 1000.0, 7.0f, 45.0f);
+	m_physparams.set_density(0, 1000.0, 7.0f, 40.0f);
 
 	m_physparams.dcoeff = 5.0f*g*H;
 
 	m_physparams.r0 = m_deltap;
 	//m_physparams.visccoeff = 0.05f;
-	m_physparams.kinematicvisc = 1.0e-6f;
-	//m_physparams.kinematicvisc = 6.0e-2f;
+	//m_physparams.kinematicvisc = 1.0e-6f;
+	m_physparams.kinematicvisc = 1.0e-2f;
 	m_physparams.artvisccoeff = 0.3f;
 	m_physparams.epsartvisc = 0.01*m_simparams.slength*m_simparams.slength;
 	m_physparams.epsxsph = 0.5f;
@@ -81,11 +114,11 @@ InputProblem::InputProblem(const Options &options) : Problem(options)
 	m_maxrho = density(H, 0);
 	m_minrho = m_physparams.rho0[0];
 	m_minvel = 0.0f;
-	m_maxvel = 0.1f;
+	m_maxvel = 0.4f;
 
 	// Drawing and saving times
 	m_displayinterval = 1.0e-4;
-	m_writefreq = 1000;
+	m_writefreq = 500;
 	m_screenshotfreq = 0;
 
 	// Name of problem used for directory creation

src/StillWater.cc
@@ -55,7 +55,7 @@ StillWater::StillWater(const Options &options) : Problem(options)
 	m_simparams.buildneibsfreq = 20;
 	m_simparams.shepardfreq = 0;
 	m_simparams.mlsfreq = 0;
-	m_simparams.ferrari = 0.1f;
+	m_simparams.ferrari = 0.1;
 	//m_simparams.visctype = KINEMATICVISC;
 	m_simparams.visctype = DYNAMICVISC;
 	//m_simparams.visctype = ARTVISC;
@@ -77,8 +77,8 @@ StillWater::StillWater(const Options &options) : Problem(options)
 
 	m_physparams.r0 = m_deltap;
 	//m_physparams.visccoeff = 0.05f;
-	//m_physparams.kinematicvisc = 3.0e-2f;
-	m_physparams.kinematicvisc = 1.0e-6f;
+	m_physparams.kinematicvisc = 3.0e-2f;
+	//m_physparams.kinematicvisc = 1.0e-6f;
 	m_physparams.artvisccoeff = 0.3f;
 	m_physparams.epsartvisc = 0.01*m_simparams.slength*m_simparams.slength;
 	m_physparams.epsxsph = 0.5f;

commit d2f513a5a007cf2bb12387444db41382fdaec5b2

Fix bug introduced in 3dbc20f4 - do not ignore non-Z components of gravity
Problem::density() was modified to ignore non-Z components of gravity,
maybe in a rushed fix to use abs()

src/Problem.cc
@@ -601,8 +601,7 @@ Problem::density(float h, int i) const
 	float density = physparams()->rho0[i];
 
 	if (h > 0) {
-		//float g = length(physparams()->gravity);
-		float g = fabsf(physparams()->gravity.z);
+		float g = fabsf(length(physparams()->gravity));
 		// TODO g*rho0*h/B could be simplified to g*h*gamma/(c0*c0)
 		density = physparams()->rho0[i]*pow(g*physparams()->rho0[i]*h/physparams()->bcoeff[i] + 1,
 				1/physparams()->gammacoeff[i]);