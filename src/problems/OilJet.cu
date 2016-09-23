55fa30d759e45ddd9b690bd59c21929b333a50aa
Agnès Leroy <agnes.leroy@edf.fr>
2016-09-23 16:38:36 +0200

    [problems] Update OilJet problem to use XProblem

--- src/problems/OilJet.cu
+++ src/problems/OilJet.cu
@@ -13,7 +13,7 @@
 #include "cudasimframework.cu"
 
 
-OilJet::OilJet(GlobalData *_gdata) : Problem(_gdata)
+OilJet::OilJet(GlobalData *_gdata) : XProblem(_gdata)
 {
 	// Data for problem setup
 	layers = 5;
@@ -83,19 +83,34 @@ OilJet::OilJet(GlobalData *_gdata) : Problem(_gdata)
 
 	// Name of problem used for directory creation
 	m_name = "OilJet";
-}
 
+	// Building the geometry
+	const int layersm1 = layers - 1;
+	setPositioning(PP_CORNER);
 
-OilJet::~OilJet(void)
-{
-	release_memory();
-}
+	GeometryID fluid1 = addBox(GT_FLUID, FT_SOLID,
+		Point(m_deltap/2, m_deltap/2, m_deltap/2),
+		lx - m_deltap, ly - m_deltap, water_level - m_deltap);
 
+	GeometryID bottom = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
+		Point(m_deltap/2, m_deltap/2, -(layersm1 + 0.5)*m_deltap),
+		lx - m_deltap, ly - m_deltap, layersm1*m_deltap);
+	disableCollisions(bottom);
 
-void OilJet::release_memory(void)
-{
-	parts_f1.clear();
-	boundary_parts_f1.clear();
+	setPositioning(PP_BOTTOM_CENTER);
+	double plength = pipe_length + layersm1*m_deltap - m_deltap/2.;
+	Point corigin = Point(lx/2., ly/2., - plength - m_deltap/2.);
+	GeometryID pipe = addCylinder(GT_FIXED_BOUNDARY, FT_BORDER,
+		corigin, (inner_diam - m_deltap)/2. + layersm1*m_deltap, plength );
+	disableCollisions(pipe);
+
+	GeometryID oil = addCylinder(GT_FLUID, FT_SOLID,
+		corigin, (inner_diam - m_deltap)/2., plength);
+
+	piston_origin = make_double3(lx/2., ly/2., - plength + layersm1*m_deltap/2.);
+	GeometryID piston = addCylinder(GT_MOVING_BODY, FT_BORDER,
+		corigin, (inner_diam - m_deltap)/2. + layersm1*m_deltap, layersm1*m_deltap);
+	disableCollisions(piston);
 }
 
 void
@@ -113,150 +128,3 @@ OilJet::moving_bodies_callback(const uint index, Object* object, const double t0
 		kdata.lvel = make_double3(0.0);
 	}
 }
-
-
-int OilJet::fill_parts()
-{
-	const int layersm1 = layers - 1;
-
-	Cube fluid1 = Cube(Point(m_deltap/2, m_deltap/2, m_deltap/2), lx - m_deltap, ly - m_deltap, water_level - m_deltap);
-	fluid1.SetPartMass(m_deltap, physparams()->rho0[0]);
-	fluid1.Fill(parts_f1, m_deltap, true);
-
-	double deltap_wall = m_deltap;
-	Cube bottom = Cube(Point(m_deltap/2, m_deltap/2, -(layersm1 + 0.5)*m_deltap),
-			lx - m_deltap, ly - m_deltap, layersm1*m_deltap);
-	bottom.SetPartMass(deltap_wall, physparams()->rho0[0]);
-	bottom.Fill(boundary_parts_f1, deltap_wall, true);
-
-	double plength = pipe_length + layersm1*m_deltap - m_deltap/2.;
-	Point corigin = Point(lx/2., ly/2., - plength - m_deltap/2.);
-	Cylinder pipe = Cylinder(corigin, (inner_diam - m_deltap)/2. + layersm1*m_deltap, plength );
-	pipe.Unfill(boundary_parts_f1, 0.4*deltap_wall);
-	pipe.SetPartMass(deltap_wall, physparams()->rho0[0]);
-	pipe.Fill(boundary_parts_f2, deltap_wall);
-
-	Cylinder oil = Cylinder(corigin, (inner_diam - m_deltap)/2., plength);
-	oil.SetPartMass(m_deltap, physparams()->rho0[0]);
-	oil.Unfill(boundary_parts_f2, m_deltap);
-	oil.Fill(parts_f2, m_deltap);
-
-	piston_origin = make_double3(lx/2., ly/2., - plength + layersm1*m_deltap/2.);
-	piston = Cylinder(corigin, (inner_diam - m_deltap)/2. + layersm1*m_deltap, layersm1*m_deltap);
-	piston.Unfill(parts_f2, m_deltap);
-	piston.SetPartMass(deltap_wall, physparams()->rho0[0]);
-	piston.Fill(piston.GetParts(), deltap_wall);
-	add_moving_body(&piston, MB_MOVING);
-	set_body_cg(&piston, piston_origin);
-
-	return parts_f1.size() + boundary_parts_f1.size() +
-			parts_f2.size() + boundary_parts_f2.size() + get_bodies_numparts();
-}
-
-
-void OilJet::copy_to_array(BufferList &buffers)
-{
-	float4 *pos = buffers.getData<BUFFER_POS>();
-	hashKey *hash = buffers.getData<BUFFER_HASH>();
-	float4 *vel = buffers.getData<BUFFER_VEL>();
-	particleinfo *info = buffers.getData<BUFFER_INFO>();
-
-	int j = 0;
-
-	cout << "\nBoundary parts fluid1: " << boundary_parts_f1.size() << "\n";
-	cout << "      " << j  << "--" << boundary_parts_f1.size() << "\n";
-	for (uint i = j; i < j + boundary_parts_f1.size(); i++) {
-		float ht = water_level - boundary_parts_f1[i-j](2);
-		if (ht < 0)
-			ht = 0.0;
-		float rho = density(ht, 0);
-		vel[i] = make_float4(0, 0, 0, rho);
-		info[i]= make_particleinfo(PT_BOUNDARY, 0, i);  // first is type, object, 3rd id
-		calc_localpos_and_hash(boundary_parts_f1[i-j], info[i], pos[i], hash[i]);
-	}
-	j += boundary_parts_f1.size();
-	cout << "Boundary part fluid1 mass:" << pos[j-1].w << "\n";
-
-	cout << "\nBoundary parts fluid2: " << boundary_parts_f2.size() << "\n";
-	cout << "      " << j  << "--" << boundary_parts_f2.size() << "\n";
-	for (uint i = j; i < j + boundary_parts_f2.size(); i++) {
-		float ht = water_level - boundary_parts_f2[i-j](2);
-		if (ht < 0)
-			ht = 0.0;
-		float rho = density(ht + 2*0.5*piston_vel/abs(physparams()->gravity.z) , 0);
-		vel[i] = make_float4(0, 0, 0, rho);
-		info[i]= make_particleinfo(PT_BOUNDARY, 0, i);  // first is type, object, 3rd id
-		calc_localpos_and_hash(boundary_parts_f2[i-j], info[i], pos[i], hash[i]);
-	}
-	j += boundary_parts_f2.size();
-	cout << "Boundary part fluid2 mass:" << pos[j-1].w << "\n";
-
-	cout << "\nFluid1 parts: " << parts_f1.size() << "\n";
-	cout << "      "<< j  << "--" << j + parts_f1.size() << "\n";
-	for (uint i = j; i < j + parts_f1.size(); i++) {
-		float ht = water_level - parts_f1[i-j](2);
-		if (ht < 0)
-			ht = 0.0;
-		float rho = density(ht, 0);
-		vel[i] = make_float4(0, 0, 0, rho);
-		info[i]= make_particleinfo(PT_FLUID, 0, i);
-		calc_localpos_and_hash(parts_f1[i-j], info[i], pos[i], hash[i]);
-	}
-	j += parts_f1.size();
-	cout << "Fluid1 part mass:" << pos[j-1].w << "\n";
-
-	cout << "\nFluid2 parts: " << parts_f2.size() << "\n";
-	cout << "      "<< j  << "--" << j + parts_f2.size() << "\n";
-	for (uint i = j; i < j + parts_f2.size(); i++) {
-		float ht = water_level - parts_f2[i-j](2);
-		if (ht < 0)
-			ht = 0.0;
-		float rho = density(ht, 0);
-		vel[i] = make_float4(0, 0, piston_vel, rho);
-		info[i]= make_particleinfo(PT_FLUID, 0, i);
-		calc_localpos_and_hash(parts_f2[i-j], info[i], pos[i], hash[i]);
-	}
-	j += parts_f2.size();
-	cout << "Fluid2 part mass:" << pos[j-1].w << "\n";
-
-	uint object_particle_counter = 0;
-	for (uint k = 0; k < m_bodies.size(); k++) {
-		PointVect & rbparts = m_bodies[k]->object->GetParts();
-		cout << "Rigid body " << k << ": " << rbparts.size() << " particles ";
-		for (uint i = 0; i < rbparts.size(); i++) {
-			uint ij = i + j;
-			float ht = water_level - rbparts[i](2);
-			if (ht < 0)
-				ht = 0.0;
-			float rho = density(ht + 2*0.5*piston_vel/abs(physparams()->gravity.z), 0);
-			//rho = physparams()->rho0[0];
-			vel[ij] = make_float4(0, 0, piston_vel, rho);
-			uint ptype = (uint) PT_BOUNDARY;
-			switch (m_bodies[k]->type) {
-				case MB_FLOATING:
-					ptype |= FG_MOVING_BOUNDARY | FG_COMPUTE_FORCE;
-					break;
-				case MB_FORCES_MOVING:
-					ptype |= FG_COMPUTE_FORCE | FG_MOVING_BOUNDARY;
-					break;
-				case MB_MOVING:
-					ptype |= FG_MOVING_BOUNDARY;
-					break;
-			}
-			info[ij] = make_particleinfo(ptype, k, ij);
-			calc_localpos_and_hash(rbparts[i], info[ij], pos[ij], hash[ij]);
-		}
-		if (k < simparams()->numforcesbodies) {
-			gdata->s_hRbFirstIndex[k] = -j + object_particle_counter;
-			gdata->s_hRbLastIndex[k] = object_particle_counter + rbparts.size() - 1;
-			object_particle_counter += rbparts.size();
-		}
-		j += rbparts.size();
-		cout << ", part mass: " << pos[j-1].w << "\n";
-	}
-
-	cout << "Everything uploaded" <<"\n";
-}
-
-
-
