7e49e927377ddfc6ce4ee62f4dd0da2d055247d1
Agnès Leroy <agnes.leroy@edf.fr>
2017-05-22 16:05:38 +0200

    [k-eps] Move default initialization for k and epsilon to XProblem
    The function init_keps was overwriting any custom initialization done
    in initializeParticles on k and epsilon. init_keps is now called
    in the XProblem copy_to_array function, just before initializeParticles.

--- src/XProblem.h
+++ src/XProblem.h
@@ -318,6 +318,9 @@ class XProblem: public Problem {
 
 		// callback for filtering out points before they become particles
 		virtual void filterPoints(PointVect &fluidParts, PointVect &boundaryParts);
+		// default initialization for k and espilon
+		virtual void init_keps(float*, float*, uint, particleinfo*, float4*, hashKey*);
+
 		// callback for initializing particles with custom values
 		virtual void initializeParticles(BufferList &buffers, const uint numParticles);
 
