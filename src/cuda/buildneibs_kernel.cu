GPUSPH/gpusph

commit 30368e554c80596b9441b4d07eaa3c063a2e67ce

Fix new particle ID generation for inlets in multi-GPU/multi-node simulations
	- the new particles IDs were not generated in a unique way in multi-GPU simulations
	- there were 2 issues:
		- the new_index value was not correctly computed because clone_idx was called with
		  numParticles (internal + external) instead of oldNumParticles (internal only)
		- the formula for new IDs was newNumPartsOnDevice*numDevices + totParticles*numDevices
		  instead of totParticles + newNumPartsOnDevice*numDevices + deviceNumber

src/cuda/buildneibs_kernel.cu
@@ -393,13 +393,14 @@ void reorderDataAndFindCellStartDevice( uint*			cellStart,			// index of cells f
 			if (cellHash != CELL_HASH_MAX)
 				// If it isn't an inactive particle, it is also the start of the cell
 				cellStart[cellHash & CELLTYPE_BITMASK] = index;
-			else
-				*newNumParticles = index;
+			//else
+			//	*newNumParticles = index;
 
 			// If it isn't the first particle, it must also be the end of the previous cell
 			if (index > 0)
 				cellEnd[sharedHash[threadIdx.x] & CELLTYPE_BITMASK] = index;
 		}
+		*newNumParticles = numParticles;
 
 		// If we are an inactive particle, we're done (short hash: compare with 32 bits max)
 		if (cellHash == CELL_HASH_MAX)
@@ -408,7 +409,7 @@ void reorderDataAndFindCellStartDevice( uint*			cellStart,			// index of cells f
 		if (index == numParticles - 1) {
 			// Ditto
 			cellEnd[cellHash & CELLTYPE_BITMASK] = index + 1;
-			*newNumParticles = numParticles;
+			//*newNumParticles = numParticles;
 		}
 
 		if (segmentStart) {

src/GPUSPH.cc
@@ -242,10 +242,13 @@ bool GPUSPH::initialize(GlobalData *_gdata) {
 
 	// allocate the particles of the *whole* simulation
 
-	// Determine the highest ID per device for unambiguous particle creation
-	for (uint d=0; d < gdata->devices; d++)
-		gdata->highestDevId[d] = gdata->totParticles + GlobalData::GLOBAL_DEVICE_ID(gdata->mpi_rank, d);
+	// Determine the initial device offset for unique particle ID creation
+	for (uint d=0; d < gdata->devices; d++) {
+		devcount_t globalDeviceIdx = GlobalData::GLOBAL_DEVICE_ID(gdata->mpi_rank, d);
+			devcount_t deviceNum = gdata->GLOBAL_DEVICE_NUM(globalDeviceIdx);
 
+		gdata->deviceIdOffset[deviceNum] = deviceNum;
+	}
 	// Allocate internal storage for moving bodies
 	problem->allocate_bodies_storage();
 

src/GPUWorker.cc
@@ -69,6 +69,7 @@ GPUWorker::GPUWorker(GlobalData* _gdata, devcount_t _deviceIndex) :
 	m_cudaDeviceNumber = gdata->device[m_deviceIndex];
 
 	m_globalDeviceIdx = GlobalData::GLOBAL_DEVICE_ID(gdata->mpi_rank, _deviceIndex);
+	m_deviceNum = gdata->GLOBAL_DEVICE_NUM(m_globalDeviceIdx);
 
 	printf("Thread 0x%zx global device id: %d (%d)\n", pthread_self(), m_globalDeviceIdx, gdata->totDevices);
 
@@ -1298,7 +1299,6 @@ void GPUWorker::downloadNewNumParticles()
 
 	uint activeParticles;
 	CUDA_SAFE_CALL(cudaMemcpy(&activeParticles, m_dNewNumParticles, sizeof(uint), cudaMemcpyDeviceToHost));
-
 	if (activeParticles > m_numAllocatedParticles) {
 		fprintf(stderr, "ERROR: Number of particles grew too much: %u > %u\n", activeParticles, m_numAllocatedParticles);
 		gdata->quit_request = true;
@@ -1309,19 +1309,6 @@ void GPUWorker::downloadNewNumParticles()
 	if (activeParticles != m_numParticles) {
 		// if for debug reasons we need to print the change in numParts for each device, uncomment the following:
 		// printf("  Dev. index %u: particles: %d => %d\n", m_deviceIndex, m_numParticles, activeParticles);
-
-		// Increment the highest particle ID that will be used as offset by the particle creation function,
-		// checking for overflow
-		if (activeParticles > m_numParticles) {
-			uint id_delta = (activeParticles-m_numParticles)*gdata->totDevices;
-			if (UINT_MAX - id_delta < gdata->highestDevId[m_deviceIndex]) {
-				fprintf(stderr, " FATAL: possible ID overflow in particle creation after iteration %lu on device %d - requesting quit...\n",
-					gdata->iterations, m_globalDeviceIdx);
-				gdata->quit_request = true;
-			}
-
-			gdata->highestDevId[m_deviceIndex] += id_delta;
-		}
 
 		m_numParticles = activeParticles;
 		// In multi-device simulations, m_numInternalParticles is updated in dropExternalParticles() and updateSegments();
@@ -1830,7 +1817,6 @@ void GPUWorker::kernel_calcHash()
 {
 	// is the device empty? (unlikely but possible before LB kicks in)
 	if (m_numParticles == 0) return;
-
 	BufferList const& bufread = *m_dBuffers.getReadBufferList();
 	BufferList &bufwrite = *m_dBuffers.getWriteBufferList();
 
@@ -2524,7 +2510,7 @@ void GPUWorker::kernel_saVertexBoundaryConditions()
 	bool initStep = (gdata->commandFlags & INITIALIZATION_STEP);
 	bool firstStep = (gdata->commandFlags & INTEGRATOR_STEP_1);
 
-	bcEngine->updateNewIDsOffset(gdata->highestDevId[m_deviceIndex]);
+	bcEngine->updateNewIDsOffset(gdata->deviceIdOffset[m_deviceNum]);
 
 	BufferList const& bufread = *m_dBuffers.getReadBufferList();
 	BufferList &bufwrite = *m_dBuffers.getWriteBufferList();
@@ -2560,7 +2546,9 @@ void GPUWorker::kernel_saVertexBoundaryConditions()
 				initStep,
 				!gdata->clOptions->resume_fname.empty(),
 				m_globalDeviceIdx,
-				gdata->totDevices);
+				gdata->totDevices,
+				gdata->totParticles
+				);
 }
 
 void GPUWorker::kernel_saIdentifyCornerVertices()

src/GPUWorker.h
@@ -72,6 +72,7 @@ private:
 	unsigned int m_cudaDeviceNumber;
 	devcount_t m_deviceIndex;
 	devcount_t m_globalDeviceIdx;
+	devcount_t m_deviceNum;
 	GlobalData* getGlobalData();
 	unsigned int getCUDADeviceNumber();
 	devcount_t getDeviceIndex();

src/GlobalData.h
@@ -200,7 +200,7 @@ struct GlobalData {
 	devcount_t devices;
 	// array of cuda device numbers
 	unsigned int device[MAX_DEVICES_PER_NODE];
-	uint highestDevId[MAX_DEVICES_PER_NODE];
+	uint deviceIdOffset[MAX_DEVICES_PER_NODE];
 
 	// MPI vars
 	devcount_t mpi_nodes; // # of MPI nodes. 0 if network manager is not initialized, 1 if no other nodes (only multi-gpu)

src/cuda/bounds_kernel.cu
@@ -213,15 +213,28 @@ createNewFluidParticle(
 	/// [in] number of devices
 	const	uint			numDevices,
 	/// [in,out] number of particles including all the ones already created in this timestep
-			uint			*newNumParticles)
+			uint			*newNumParticles,
+	const uint			totParticles)
 {
 	const uint new_index = atomicAdd(newNumParticles, 1);
-	// number of new particles that were created on this device in this
-	// time step
+	// number of new particles that were created on this device
+	// in this time step
 	const uint newNumPartsOnDevice = new_index + 1 - numParticles;
-	// the i-th device can only allocate an id that satisfies id%n == i, where
-	// n = number of total devices
-	const uint new_id = newNumPartsOnDevice*numDevices + d_newIDsOffset;
+	if (UINT_MAX - newNumPartsOnDevice*numDevices < totParticles + d_newIDsOffset) {
+		printf(" FATAL: possible ID overflow in particle creation on device %d, your simulation may crash\n", d_newIDsOffset);
+	}
+	// ID of the new particle. Must be unique across all the GPUs: it is set
+	// as the total number of particles (N) + the chosen offset (ie the device global number, G)
+	// + the number of new particles on the device (k) times the total number of devices (D)
+	// New_id = N + G + kD
+	// Let's say for example that the simulation starts with 1M particles,
+	// and that there are 3 devices (so N=10^6 and D=3),
+	// with global device number G=0,1,2. Then the IDs created by each device would be:
+	// for device 0: 1M + 3, 1M + 6, 1M + 9, ...
+	// for device 1: 1M + 4, 1M + 7, 1M + 10, ...
+	// for device 2: 1M + 5, 1M + 8, 1M + 11, ...
+
+	const uint new_id = totParticles + newNumPartsOnDevice*numDevices + d_newIDsOffset;
 
 	new_info = make_particleinfo_by_ids(
 		PT_FLUID,

src/cuda/forces.cu
@@ -1157,7 +1157,8 @@ saVertexBoundaryConditions(
 	const	bool			initStep,
 	const	bool			resume,
 	const	uint			deviceId,
-	const	uint			numDevices)
+	const	uint			numDevices,
+	const uint			totParticles)
 {
 	int dummy_shared = 0;
 
@@ -1174,7 +1175,8 @@ saVertexBoundaryConditions(
 	// execute the kernel
 	cuforces::saVertexBoundaryConditions<kerneltype><<< numBlocks, numThreads, dummy_shared >>>
 		(oldPos, oldVel, oldTKE, oldEps, oldGGam, oldEulerVel, forces, contupd, vertices, vertPos[0], vertPos[1], vertPos[2], info, particleHash, cellStart, neibsList,
-		 particleRangeEnd, newNumParticles, dt, step, deltap, slength, influenceradius, initStep, resume, deviceId, numDevices);
+		 particleRangeEnd, numParticles, newNumParticles, dt, step, deltap, slength, influenceradius, initStep, resume, deviceId, numDevices, 
+		 totParticles);
 
 	// check if kernel invocation generated an error
 	KERNEL_CHECK_ERROR;

src/cuda/forces_kernel.cu
@@ -1320,6 +1320,7 @@ saVertexBoundaryConditions(
 				const	uint*			cellStart,
 				const	neibdata*		neibsList,
 				const	uint			numParticles,
+				const	uint			oldNumParticles,
 						uint*			newNumParticles,
 				const	float			dt,
 				const	int				step,
@@ -1329,7 +1330,8 @@ saVertexBoundaryConditions(
 				const	bool			initStep,
 				const	bool			resume,
 				const	uint			deviceId,
-				const	uint			numDevices)
+				const	uint			numDevices,
+				const	uint			totParticles)
 {
 	const uint index = INTMUL(blockIdx.x,blockDim.x) + threadIdx.x;
 
@@ -1630,7 +1632,7 @@ saVertexBoundaryConditions(
 			massFluid -= refMass;
 			// Create new particle
 			particleinfo clone_info;
-			uint clone_idx = createNewFluidParticle(clone_info, info, numParticles, numDevices, newNumParticles);
+			uint clone_idx = createNewFluidParticle(clone_info, info, oldNumParticles, numDevices, newNumParticles, totParticles);
 
 			// Problem has already checked that there is enough memory for new particles
 			float4 clone_pos = pos; // new position is position of vertex particle

commit c87d504992ba9a3c7f04696bf974f220965e82b4

Fix bug introduced in commit 30368e554c80596b9441b4d07eaa3c063a2e67ce

src/cuda/buildneibs_kernel.cu
@@ -393,14 +393,13 @@ void reorderDataAndFindCellStartDevice( uint*			cellStart,			// index of cells f
 			if (cellHash != CELL_HASH_MAX)
 				// If it isn't an inactive particle, it is also the start of the cell
 				cellStart[cellHash & CELLTYPE_BITMASK] = index;
-			//else
-			//	*newNumParticles = index;
+			else
+				*newNumParticles = index;
 
 			// If it isn't the first particle, it must also be the end of the previous cell
 			if (index > 0)
 				cellEnd[sharedHash[threadIdx.x] & CELLTYPE_BITMASK] = index;
 		}
-		*newNumParticles = numParticles;
 
 		// If we are an inactive particle, we're done (short hash: compare with 32 bits max)
 		if (cellHash == CELL_HASH_MAX)
@@ -409,7 +408,7 @@ void reorderDataAndFindCellStartDevice( uint*			cellStart,			// index of cells f
 		if (index == numParticles - 1) {
 			// Ditto
 			cellEnd[cellHash & CELLTYPE_BITMASK] = index + 1;
-			//*newNumParticles = numParticles;
+			*newNumParticles = numParticles;
 		}
 
 		if (segmentStart) {