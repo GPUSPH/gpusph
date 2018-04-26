GPUSPH/gpusph

commit 21884dafe4febabf9fd6c7c202aaa02b36b282f1

Start using for_each_neib
The aim is to reduce the boilerplate and thus make it easier to avoid
errors when coding the traversal.

Most major kernels are converted. Some optional ones and most of the
ones that need to iterate over multiple types are kept as-is.

src/cuda/boundary_conditions_kernel.cu
@@ -389,31 +389,17 @@ saSegmentBoundaryConditions(			float4*		__restrict__ oldPos,
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
 	// Persistent variables across getNeibData calls
-	char neib_cellnum;
-	uint neib_cell_base_index;
-	float3 pos_corr;
-	idx_t i;
 
 	// Loop over VERTEX neighbors.
 	// TODO this is only needed
 	// (1) to compute gamma
 	// (2) to compute the velocity for boundary of moving objects
 	// (3) to compute the eulerian velocity for non-IO boundaries in the KEPS case
-	neib_cellnum = 0;
-	neib_cell_base_index = 0;
-	i = (d_neibboundpos + 1)*d_neiblist_stride;
-	while (true) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-		i += d_neiblist_stride;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	for_each_neib(PT_VERTEX, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		// Compute relative position vector and distance
-		// Now relPos is a float4 and neib mass is stored in relPos.w
-		const float4 relPos = pos_corr - oldPos[neib_index];
+		const float4 relPos = neib_iter.relPos(oldPos[neib_index]);
 
 		// skip inactive particles
 		if (INACTIVE(relPos))
@@ -441,6 +427,7 @@ saSegmentBoundaryConditions(			float4*		__restrict__ oldPos,
 		oldGGam[index] = gGam;
 		gGam.w = fmaxf(gGam.w, 1e-5f);
 	}
+
 	// finalize velocity computation. we only store it later though, because the rest of this
 	// kernel may compute vel.w
 	vel.x /= 3;
@@ -449,21 +436,12 @@ saSegmentBoundaryConditions(			float4*		__restrict__ oldPos,
 
 
 	// Loop over FLUID neighbors
-	neib_cellnum = 0;
-	neib_cell_base_index = 0;
-	i = 0;
-	while (true) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-		i += d_neiblist_stride;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	for_each_neib(PT_FLUID, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		// Compute relative position vector and distance
 		// Now relPos is a float4 and neib mass is stored in relPos.w
-		const float4 relPos = pos_corr - oldPos[neib_index];
+		const float4 relPos = neib_iter.relPos(oldPos[neib_index]);
 
 		// skip inactive particles
 		if (INACTIVE(relPos))
@@ -639,21 +617,9 @@ computeVertexNormal(
 	// Compute grid position of current particle
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
-	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
-	float3 pos_corr;
-
 	// Loop over all BOUNDARY neighbors
-	idx_t i = d_neibboundpos*d_neiblist_stride;
-	while (true) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-		i -= d_neiblist_stride;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 		const particleinfo neib_info = pinfo[neib_index];
 
 		// Skip this neighboring boundary element if it's not in the same boundary
@@ -741,24 +707,11 @@ initGamma(
 	// Compute grid position of current particle
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
-	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
-	float3 pos_corr;
-
 	// Iterate over all BOUNDARY neighbors
-	idx_t i = d_neibboundpos*d_neiblist_stride;
+	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
-	while (true) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-		i -= d_neiblist_stride;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
-
-		const float3 relPos = pos_corr - as_float3(oldPos[neib_index]);
+		const float3 relPos = as_float3(neib_iter.relPos(oldPos[neib_index]));
 
 		if (length(relPos) > influenceradius + deltap*0.5f)
 			continue;
@@ -844,30 +797,22 @@ initIOmass_vertexCount(
 		return;
 
 	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
 	uint vertexCount = 0;
 
 	const float4 pos = make_float4(0.0f); // we don't need pos, so let's just set it to 0
-	float3 pos_corr;
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
 	uint neibVertIds[MAXNEIBVERTS];
 	uint neibVertIdsCount=0;
 
-	// Loop over all the neighbors
-	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// Loop over all BOUNDARY neighbors
+	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		const particleinfo neib_info = pinfo[neib_index];
 
-		// only boundary neighbours as we need to count the vertices that belong to the same segment as our vertex particle
-		if (BOUNDARY(neib_info) && IO_BOUNDARY(neib_info)) {
+		// only IO boundary neighbours as we need to count the vertices that belong to the same segment as our vertex particle
+		if (IO_BOUNDARY(neib_info)) {
 
 			// prepare ids of neib vertices
 			const vertexinfo neibVerts = vertices[neib_index];
@@ -892,22 +837,11 @@ initIOmass_vertexCount(
 		}
 	}
 
-	neib_cellnum = 0;
-	neib_cell_base_index = 0;
-
-	// Loop over all the neighbors
-	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// Loop over all VERTEX neighbors
+	for_each_neib(PT_VERTEX, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		const particleinfo neib_info = pinfo[neib_index];
-
-		if (!VERTEX(neib_info))
-			continue;
 
 		for (uint j = 0; j<neibVertIdsCount; j++) {
 			if (id(neib_info) == neibVertIds[j] && !CORNER(neib_info))
@@ -948,11 +882,6 @@ initIOmass(
 	if (!(VERTEX(info) && IO_BOUNDARY(info) && !CORNER(info)))
 		return;
 
-	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
-	float3 pos_corr;
-
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
 	// does this vertex get or donate mass; decided by the id of a vertex particle
@@ -969,59 +898,42 @@ initIOmass(
 	uint neibVertIds[MAXNEIBVERTS];
 	uint neibVertIdsCount=0;
 
-	// Loop over all the neighbors
-	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// Loop over all BOUNDARY neighbors
+	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		const particleinfo neib_info = pinfo[neib_index];
 
-		// only boundary neighbours as we need to count the vertices that belong to the same segment as our vertex particle
-		if (BOUNDARY(neib_info) && IO_BOUNDARY(neib_info)) {
+		// only IO boundary neighbours as we need to count the vertices that belong to the same segment as our vertex particle
+		if (!IO_BOUNDARY(neib_info))
+			continue;
 
-			// prepare ids of neib vertices
-			const vertexinfo neibVerts = vertices[neib_index];
+		// prepare ids of neib vertices
+		const vertexinfo neibVerts = vertices[neib_index];
 
-			// only check adjacent boundaries
-			if (neibVerts.x == id(info) || neibVerts.y == id(info) || neibVerts.z == id(info)) {
-				// check if we don't have the current vertex
-				if (id(info) != neibVerts.x) {
-					neibVertIds[neibVertIdsCount] = neibVerts.x;
-					neibVertIdsCount+=1;
-				}
-				if (id(info) != neibVerts.y) {
-					neibVertIds[neibVertIdsCount] = neibVerts.y;
-					neibVertIdsCount+=1;
-				}
-				if (id(info) != neibVerts.z) {
-					neibVertIds[neibVertIdsCount] = neibVerts.z;
-					neibVertIdsCount+=1;
-				}
+		// only check adjacent boundaries
+		if (neibVerts.x == id(info) || neibVerts.y == id(info) || neibVerts.z == id(info)) {
+			// check if we don't have the current vertex
+			if (id(info) != neibVerts.x) {
+				neibVertIds[neibVertIdsCount] = neibVerts.x;
+				neibVertIdsCount+=1;
 			}
-
+			if (id(info) != neibVerts.y) {
+				neibVertIds[neibVertIdsCount] = neibVerts.y;
+				neibVertIdsCount+=1;
+			}
+			if (id(info) != neibVerts.z) {
+				neibVertIds[neibVertIdsCount] = neibVerts.z;
+				neibVertIdsCount+=1;
+			}
 		}
 	}
 
-	neib_cellnum = 0;
-	neib_cell_base_index = 0;
-
-	// Loop over all the neighbors
-	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// Loop over all VERTEX neighbors
+	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		const particleinfo neib_info = pinfo[neib_index];
-
-		if (!VERTEX(neib_info))
-			continue;
 
 		for (uint j = 0; j<neibVertIdsCount; j++) {
 			if (id(neib_info) == neibVertIds[j]) {
@@ -1134,6 +1046,7 @@ saVertexBoundaryConditions(
 	idx_t i = 0;
 
 	// Loop over all the neighbors
+	// TODO FIXME splitneibs merge : check logic against master
 	while (true) {
 		neibdata neib_data = neibsList[i + index];
 
@@ -1200,27 +1113,17 @@ saIdentifyCornerVertices(
 	// Compute grid position of current particle
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
-	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
-	float3 pos_corr;
-
 	const uint vid = id(info);
 
-	// Loop over all the neighbors
-	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == 0xffff) break;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// Loop over all BOUNDARY neighbors
+	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		const particleinfo neib_info = pinfo[neib_index];
 		const uint neib_obj = object(neib_info);
 
 		// loop only over boundary elements that are not of the same open boundary
-		if (BOUNDARY(neib_info) && !(obj == neib_obj && IO_BOUNDARY(neib_info))) {
+		if (!(obj == neib_obj && IO_BOUNDARY(neib_info))) {
 			// check if the current vertex is part of the vertices of the segment
 			if (vertices[neib_index].x == vid ||
 				vertices[neib_index].y == vid ||

src/cuda/density_sum_kernel.cu
@@ -241,19 +241,9 @@ computeDensitySumBoundaryTerms(
 	// Compute grid position of current particle
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
-	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
-	float3 pos_corr;
-
-	// Loop over boundary neighbors
-	for (idx_t i = d_neibboundpos*d_neiblist_stride; i > 0; i -= d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-
-		const uint neib_index = getNeibIndex(posN, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// Loop over BOUNDARY neighbors
+	for_each_neib(PT_BOUNDARY, index, posN, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 		const particleinfo neib_info = pinfo[neib_index];
 
 		const float4 posN_neib = oldPos[neib_index];
@@ -263,9 +253,9 @@ computeDensitySumBoundaryTerms(
 		const float4 posNp1_neib = newPos[neib_index];
 
 		// vector r_{ab} at time N
-		const float4 qN = (pos_corr - posN_neib)/slength;
+		const float4 qN = neib_iter.relPos(posN_neib)/slength;
 		// vector r_{ab} at time N+1 = r_{ab}^N + (r_a^{N+1} - r_a^{N}) - (r_b^{N+1} - r_b^N)
-		const float4 qNp1 = (make_float4(pos_corr) + posNp1 - posN - posNp1_neib)/slength;
+		const float4 qNp1 = (neib_iter.relPos(posN) + posNp1 - posNp1_neib)/slength;
 
 		// normal of segment
 		const float3 ns = as_float3(boundElement[neib_index]);

src/cuda/forces_kernel.cu
@@ -480,10 +480,12 @@ SPSstressMatrixDevice(sps_params<kerneltype, boundarytype, simflags> params)
 	float3 pos_corr;
 
 	// loop over all the neighbors
+	// TODO FIXME splitneibs : correctly iterate over all particle types OR
+	// filter based on particle type (only FLUID, BOUNDARY only for DYN?)
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = params.neibsList[i + index];
 
-		if (neib_data == 0xffff) break;
+		if (neib_data == NEIBS_END) break;
 
 		const uint neib_index = getNeibIndex(pos, pos_corr, params.cellStart,
 				neib_data, gridPos, neib_cellnum, neib_cell_base_index);
@@ -632,10 +634,12 @@ densityGrenierDevice(
 	bool has_fluid_neibs = false;
 
 	// Loop over all neighbors
+	// TODO FIXME splitneibs : this should be two loops, one on FLUID and one on BOUNDARY
+	// particles, with the latter only in the DYN case.
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
 
-		if (neib_data == 0xffff) break;
+		if (neib_data == NEIBS_END) break;
 
 		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
 			neib_cellnum, neib_cell_base_index);
@@ -765,6 +769,8 @@ shepardDevice(	const float4*	posArray,
 	float3 pos_corr;
 
 	// Loop over all the neighbors
+	// TODO FIXME splitneibs : this should be two loops, one on FLUID and one on BOUNDARY
+	// particles, with the latter only in the DYN case.
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
 
@@ -871,6 +877,8 @@ MlsDevice(	const float4*	posArray,
 	float3 pos_corr;
 
 	// First loop over all neighbors
+	// TODO FIXME splitneibs : this should be two loops, one on FLUID and one on BOUNDARY
+	// particles, with the latter only in the DYN case.
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
 
@@ -974,6 +982,8 @@ MlsDevice(	const float4*	posArray,
 	vel.w = B.x*W<kerneltype>(0, slength)*pos.w;
 
 	// Loop over all the neighbors (Second loop)
+	// TODO FIXME splitneibs : this should be two loops, one on FLUID and one on BOUNDARY
+	// particles, with the latter only in the DYN case.
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
 

src/cuda/forces_kernel.def
@@ -3003,21 +3003,12 @@ forcesDevice(
 
 		/* And finally the neighbors list traversal support */
 
-		// Persistent variables across getNeibData calls
-		char neib_cellnum = 0;
-		uint neib_cell_base_index = 0;
-		float3 pos_corr;
-
 		// Under some conditions, some particles might want to skip the
 		// neighbor list traversal. This is checked by the check() function of
 		// the skip_neiblist struct. Any action that needs to be done then is
 		// done by the prepare() function in the same struct.
-		// Setting the neib list iterator counter i to d_neiblist_end to
-		// actually skip the neib list traversal is done in here rather than
-		// in the prepare() function.
 
 		skip_neiblist<boundarytype, cptype, nptype> skip;
-		idx_t i = 0;
 
 		bool cont = true;
 		if (skip.check(params, pdata)) {
@@ -3025,28 +3016,19 @@ forcesDevice(
 			cont = false; // Skip neighbors loop
 		}
 
-		// Loop over all neighbors
-		if (nptype == PT_BOUNDARY)
-			i = d_neibboundpos *d_neiblist_stride;
-		else if (nptype == PT_VERTEX)
-			i = (d_neibboundpos + 1)*d_neiblist_stride;
-
-		while (cont) {
-			neibdata neib_data = params.neibsList[i + index];
-
-			if (neib_data == NEIBS_END) break;
-			i += (nptype == PT_BOUNDARY) ? -d_neiblist_stride : d_neiblist_stride;
-
-			const uint neib_index = getNeibIndex(pdata.pos, pos_corr, params.cellStart,
-				neib_data, pdata.gridPos, neib_cellnum, neib_cell_base_index);
+		// Loop over all neighbors of type nptype
+		if (cont) for_each_neib(nptype, index, pdata.pos, pdata.gridPos, params.cellStart, params.neibsList) {
+			const uint neib_index = neib_iter.neib_index();
 
 			// Compute relative position vector and distance
 			// WARNING: relPos is a float4 and neib mass is stored in relPos.w
+			const float4 relPos = neib_iter.relPos(
 			#if PREFER_L1
-			const float4 relPos = pos_corr - params.posArray[neib_index];
+				params.posArray[neib_index]
 			#else
-			const float4 relPos = pos_corr - tex1Dfetch(posTex, neib_index);
+				tex1Dfetch(posTex, neib_index)
 			#endif
+				);
 
 			// Skip inactive particles
 			if (INACTIVE(relPos))

src/cuda/post_process_kernel.cu
@@ -90,27 +90,19 @@ calcVortDevice(	const	float4*		posArray,
 	// Compute grid position of current particle
 	const int3 gridPos = calcGridPosFromParticleHash( particleHash[index] );
 
-	// Persistent variables across getNeibData calls
-	char neib_cellnum = 0;
-	uint neib_cell_base_index = 0;
-	float3 pos_corr;
-
-	// First loop over all neighbors
-	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
-		neibdata neib_data = neibsList[i + index];
-
-		if (neib_data == NEIBS_END) break;
-
-		const uint neib_index = getNeibIndex(pos, pos_corr, cellStart, neib_data, gridPos,
-					neib_cellnum, neib_cell_base_index);
+	// First loop over all FLUID neighbors
+	for_each_neib(PT_FLUID, index, pos, gridPos, cellStart, neibsList) {
+		const uint neib_index = neib_iter.neib_index();
 
 		// Compute relative position vector and distance
 		// Now relPos is a float4 and neib mass is stored in relPos.w
+		const float4 relPos = neib_iter.relPos(
 		#if PREFER_L1
-		const float4 relPos = pos_corr - posArray[neib_index];
+			posArray[neib_index]
 		#else
-		const float4 relPos = pos_corr - tex1Dfetch(posTex, neib_index);
+			tex1Dfetch(posTex, neib_index)
 		#endif
+			);
 
 		// skip inactive particles
 		if (INACTIVE(relPos))
@@ -124,7 +116,7 @@ calcVortDevice(	const	float4*		posArray,
 		const particleinfo neib_info = tex1Dfetch(infoTex, neib_index);
 
 		// Compute vorticity
-		if (r < influenceradius && FLUID(neib_info)) {
+		if (r < influenceradius) {
 			const float f = F<kerneltype>(r, slength)*relPos.w/relVel.w;	// ∂Wij/∂r*Vj
 			// vxij = vxi - vxj and same for vyij and vzij
 			vort.x += f*(relVel.y*relPos.z - relVel.z*relPos.y);		// vort.x = ∑(vyij(zi - zj) - vzij*(yi - yj))*∂Wij/∂r*Vj
@@ -184,6 +176,8 @@ calcTestpointsVelocityDevice(	const float4*	oldPos,
 	float3 pos_corr;
 
 	// First loop over all neighbors
+	// TODO FIXME splitneibs : this should be two loops, one on FLUID and one on VERTEX
+	// particles, with the latter only in the SA case.
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
 
@@ -303,6 +297,7 @@ calcSurfaceparticleDevice(	const	float4*			posArray,
 	float3 pos_corr;
 
 	// First loop over all neighbors
+	// TODO FIXME splitneibs : correctly iterate over all particle types
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
 
@@ -358,6 +353,7 @@ calcSurfaceparticleDevice(	const	float4*			posArray,
 	neib_cell_base_index = 0;
 
 	// loop over all the neighbors (Second loop)
+	// TODO FIXME splitneibs : correctly iterate over all particle types
 	int nc = 0;
 	for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 		neibdata neib_data = neibsList[i + index];
@@ -415,6 +411,8 @@ calcSurfaceparticleDevice(	const	float4*			posArray,
 //! Compute a private variable
 /*!
  This function computes an arbitrary passive array. It can be used for debugging purposes or passive scalars
+
+ \todo this should be defined by the problem, not here
 */
 __global__ void
 calcPrivateDevice(	const	float4*		pos_array,
@@ -448,6 +446,7 @@ calcPrivateDevice(	const	float4*		pos_array,
 		priv[index] = 0;
 
 		// Loop over all the neighbors
+		// TODO FIXME splitneibs : correctly iterate over all particle types
 		for (idx_t i = 0; i < d_neiblist_end; i += d_neiblist_stride) {
 			neibdata neib_data = neibsList[i + index];
 

commit e5c0b9a6397b6ea18a3f61b218570791fc693e3a

initIOmass was iterating over the wrong neib type
Fix stupid bug I introduced in 21884dafe4febabf9fd6c7c202aaa02b36b282f1

src/cuda/boundary_conditions_kernel.cu
@@ -1671,7 +1671,7 @@ initIOmass(
 	}
 
 	// Loop over all VERTEX neighbors
-	for_each_neib(PT_BOUNDARY, index, pos, gridPos, cellStart, neibsList) {
+	for_each_neib(PT_VERTEX, index, pos, gridPos, cellStart, neibsList) {
 		const uint neib_index = neib_iter.neib_index();
 
 		const particleinfo neib_info = pinfo[neib_index];