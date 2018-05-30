d985f1fb7f7f21382b50c964ed2296ac10c5d7c8
Alex Ghaitanellis <alex.ghaitanellis@gmail.com>
2018-05-30 10:54:50 +0800

    Implementation of Hu & Adams's formulation (WIP).

--- src/particledefine.h
+++ src/particledefine.h
@@ -116,6 +116,7 @@ enum SPHFormulation {
 	SPH_F1 = 1,
 	SPH_F2,
 	SPH_GRENIER,
+	SPH_HA,
 	SPH_INVALID
 } ;
 
@@ -130,6 +131,7 @@ const char* SPHFormulationName[SPH_INVALID+1]
 	"F1",
 	"F2",
 	"Grenier",
+	"Hu and Adams",
 	"(invalid)"
 }
 #endif
