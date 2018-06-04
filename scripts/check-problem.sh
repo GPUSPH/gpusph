603aaf568c616bafa8c34836420b9c7131fa84f1
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-06-04 12:27:29 +0200

    Script to check a single problem

---
+++ scripts/check-problem.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Run a specific problem for maxiter iterations and check against the reference
+# Syntax: check-problem problem [reference_suffix] [maxiter]
+# default reference_suffix is 'reference'
+# default maxiter is 1000
+
+abort() {
+	echo "$@" >&2
+	exit 1
+}
+
+sfx=check
+problem="$1"
+ref="$2"
+maxiter="$3"
+
+[ -z "$ref" ] && ref=reference
+[ -z "$maxiter" ] && maxiter=1000
+
+echo "Testing ${problem} ..."
+outdir="tests/${problem}_${sfx}"
+refdir="tests/${problem}_${ref}"
+
+rm -rf "$outdir"
+
+make $problem && ./GPUSPH --dir "$outdir" --maxiter $maxiter || abort "Failed!"
+diff -q "${outdir}/data" "${refdir}/data" || abort "$problem differs!"
