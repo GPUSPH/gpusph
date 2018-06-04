f44352d260ec5b441e04357b25d8ac164400594a
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-06-04 12:25:49 +0200

    check-all-problems: really check all of them
    Instead of aborting on the first error, check all problems and print a
    summary of the failures at the end. Also don't arbitrarily skip
    problems (even if we know they would not be stable). Managing a
    skip-list may be implemented later in a different way.

--- scripts/check-all-problems.sh
+++ scripts/check-all-problems.sh
@@ -5,30 +5,39 @@
 # default reference_suffix is 'reference'
 # default maxiter is 1000
 
-abort() {
-	echo "$@" >&2
-	exit 1
+failed=
+
+add_failed() {
+	failed="${failed}$1 ($2)\n"
 }
 
 sfx=check
-ref="$2"
-maxiter="$3"
+ref="$1"
+maxiter="$2"
 
 [ -z "$ref" ] && ref=reference
 [ -z "$maxiter" ] && maxiter=1000
 
-rm -rf tests/*_${sfx}
-
 for problem in $(make list-problems) ; do
-	case $problem in
-	CompleteSaExample)
-		echo "Skipping ${problem}, known non-reproducible"
-		continue
-		;;
-	esac
 	echo "Testing ${problem} ..."
 	outdir="tests/${problem}_${sfx}"
 	refdir="tests/${problem}_${ref}"
-	make $problem && ./GPUSPH --dir "$outdir" --maxiter $maxiter || abort "Failed!"
-	diff -q "${outdir}/data" "${refdir}/data" || abort "$problem differs!"
+	rm -rf "$outdir"
+	if make $problem ; then
+		if ./GPUSPH --dir "$outdir" --maxiter $maxiter ; then
+			diff -q "${outdir}/data" "${refdir}/data" || add_failed "$problem" diff
+		else
+			add_failed "$problem" run
+		fi
+	else
+		add_failed "$problem" build
+	fi
 done
+
+if [ -z "$failed" ] ; then
+	echo "All OK"
+	exit 0
+else
+	echo "Failures:\n${failed}"
+	exit 1
+fi
