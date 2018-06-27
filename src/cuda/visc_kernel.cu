b72c405faf0742472c7acdaf1b902326b7f2ed8c
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-06-27 16:16:45 +0200

    Move the viscous force magnitude functions to visc_kernel
    All viscosity-related functions should stay there.

--- src/cuda/visc_kernel.cu
+++ src/cuda/visc_kernel.cu
@@ -63,6 +63,68 @@ using namespace cuphys;
 using namespace cuneibs;
 
 /************************************************************************************************************/
+/*		   Device functions to compute the laminar viscous contributions                                    */
+/************************************************************************************************************/
+
+//! Artificial viscosity
+__device__ __forceinline__ float
+artvisc(	const float	vel_dot_pos,
+			const float	rho,
+			const float	neib_rho,
+			const float	sspeed,
+			const float	neib_sspeed,
+			const float	r,
+			const float	slength)
+{
+	return vel_dot_pos*slength*d_artvisccoeff*(sspeed + neib_sspeed)/
+			((r*r + d_epsartvisc)*(rho + neib_rho));
+}
+
+
+// ATTENTION: for all non artificial viscosity
+// µ is the dynamic viscosity (ρν)
+
+//! Morris laminar viscous term
+/*!
+ Scalar part of viscosity using Morris 1997
+ expression 21 p218 when all particles have the same viscosity
+ in this case d_visccoeff = 4 nu
+ returns 4.mj.nu/(ρi + ρj) (1/r ∂Wij/∂r)
+*/
+__device__ __forceinline__ float
+laminarvisc_kinematic(	const float	rho,
+						const float	neib_rho,
+						const float	neib_mass,
+						const float	f)
+{
+	// NOTE: this won't work in multi-fluid!
+	// TODO FIXME kinematic viscosity should probably be marked as incompatible
+	// with multi-fluid (or at least if fluids don't have the same, constant
+	// viscosity
+	return neib_mass*d_visccoeff[0]*f/(rho + neib_rho);
+}
+
+
+//! Morris laminar viscous term for variable viscosity
+/*!
+ Same behaviour as laminarvisc_kinematic but for particle
+ dependent viscosity.
+ returns mj.(µi + µi)/(ρi.ρj) (1/r ∂Wij/∂r)
+*/
+__device__ __forceinline__ float
+laminarvisc_dynamic(const float	rho,
+					const float	neib_rho,
+					const float	neib_mass,
+					const float	f,
+					const float	visc,
+					const float	neib_visc)
+{
+	return neib_mass*(visc + neib_visc)*f/(rho*neib_rho);
+}
+/************************************************************************************************************/
+
+
+/************************************************************************************************************/
 /*		   Kernels for computing SPS tensor and SPS viscosity												*/
 /************************************************************************************************************/
 
