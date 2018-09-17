b0f14f74fca7a8f9cd54787837a8e9f557639c5a
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-09-17 17:18:31 +0200

    paraview-bbox-example: improve support for multifluid

--- scripts/paraview-bbox-example.py
+++ scripts/paraview-bbox-example.py
@@ -30,7 +30,33 @@ fluid = Threshold(vtp)
 fluid.Scalars = 'Part type'
 fluid.ThresholdRange = [0, 0]
 
-print "#time,xmin,xmax,ymin,ymax,zmin,zmax,rhomin,rhomax"
+fluid.UpdatePipeline(timestep[0])
+
+array_names = [a[0] for a in fluid.PointData.items()]
+
+if 'Fluid number' in array_names:
+    fluidnum_range = fluid.PointData.GetArray('Fluid number').GetRange()
+    nfluids = int(fluidnum_range[1]) + 1
+    fluids = []
+    for f in range(0, nfluids):
+        afluid = Threshold(fluid)
+        afluid.Scalars = 'Fluid number'
+        afluid.ThresholdRange = [f, f]
+        fluids.append(afluid)
+else:
+    nfluids = 1
+    fluids = [fluid]
+
+header = "#time,xmin,xmax,ymin,ymax,zmin,zmax,rhomin,rhomax"
+fmt = "%f,%f,%f,%f,%f,%f,%f,%f,%f"
+endfmt = "**,%f,%f,%f,%f,%f,%f,%f,%f"
+if nfluids > 1:
+    for f in range(0, nfluids):
+        header += ",xmin{0},xmax{0},ymin{0},ymax{0},zmin{0},zmax{0},rhomin{0},rhomax{0}".format(f)
+        fmt += ",%f,%f,%f,%f,%f,%f,%f,%f"
+        endfmt += ",%f,%f,%f,%f,%f,%f,%f,%f"
+
+print header
 
 first = None
 last = None
@@ -40,9 +66,15 @@ for time in timestep:
     fluid.UpdatePipeline(time)
     bounds =    fluid.GetDataInformation().GetBounds()[:] + \
                 fluid.PointData.GetArray('Density').GetRange()
+    if nfluids > 1:
+        for f in range(0, nfluids):
+            afluid = fluids[f]
+            afluid.UpdatePipeline(time)
+            bounds += afluid.GetDataInformation().GetBounds()[:] + \
+                     afluid.PointData.GetArray('Density').GetRange()
     if not first:
         first = bounds
     last = bounds
-    print "%f,%f,%f,%f,%f,%f,%f,%f,%f" % ((time,) + bounds)
+    print fmt % ((time,) + bounds)
 
-print "**,%f,%f,%f,%f,%f,%f,%f,%f" % tuple(pair[1] - pair[0] for pair in zip(first, last))
+print endfmt % tuple(pair[1] - pair[0] for pair in zip(first, last))
