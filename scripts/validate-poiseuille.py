1ce4f2213a3b48c27329430616fd6937c849586d
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2018-11-23 12:44:37 +0100

    Poiseuille validation script: test all visc model

--- scripts/validate-poiseuille.py
+++ scripts/validate-poiseuille.py
@@ -24,6 +24,9 @@ driving_force=args.driving_force
 
 gnuplot = args.gnuplot
 
+compvisc_range = ("kin", "dyn")
+avgop_range = ("arithmetic", "harmonic", "geometric")
+
 ppH_range = (ref_ppH/2, ref_ppH, ref_ppH*2)
 
 def compute_poiseuille_vel(depth):
@@ -52,70 +55,76 @@ subprocess.check_call(['make', 'Poiseuille'])
 
 errors = []
 
-for ppH in ppH_range:
+for compvisc in compvisc_range:
+    for avgop in avgop_range:
+        for ppH in ppH_range:
 
-    test_name="Poiseuille-%d-%g-%g-%g" % (ppH, rho, kinvisc, driving_force)
+            test_name="Poiseuille-%s-%s-%d-%g-%g-%g" % (compvisc, avgop, ppH, rho, kinvisc, driving_force)
 
-    data_dir="tests/%s" % test_name
+            data_dir="tests/%s" % test_name
 
-    print "\n%sTest %s ...\n" % (pfx, test_name)
+            print "\n%sTest %s ...\n" % (pfx, test_name)
 
-    if not os.path.exists(data_dir):
-        with open(test_name + '.log', 'w') as logfile:
-            subprocess.check_call(['./GPUSPH', '--ppH', str(ppH), '--dir', data_dir], stdout=logfile)
+            if not os.path.exists(data_dir):
+                with open(test_name + '.log', 'w') as logfile:
+                    subprocess.check_call(['./GPUSPH',
+                        '--compvisc', compvisc,
+                        '--viscavg', avgop,
+                        '--ppH', str(ppH),
+                        '--dir', data_dir], stdout=logfile)
 
-    print "%s ... done" % pfx
+            print "%s ... done" % pfx
 
-    vtp = OpenDataFile(data_dir + "/data/VTUinp.pvd")
+            vtp = OpenDataFile(data_dir + "/data/VTUinp.pvd")
 
-    # Filter by particle type
-    t = Threshold(vtp)
-    t.Scalars = 'Part type'
-    t.ThresholdRange = [0, 0]
+            # Filter by particle type
+            t = Threshold(vtp)
+            t.Scalars = 'Part type'
+            t.ThresholdRange = [0, 0]
 
-    # Go to last step
-    t.UpdatePipeline(vtp.TimestepValues[-1])
+            # Go to last step
+            t.UpdatePipeline(vtp.TimestepValues[-1])
 
-    fdata = servermanager.Fetch(t)
-    pts = fdata.GetNumberOfPoints()
+            fdata = servermanager.Fetch(t)
+            pts = fdata.GetNumberOfPoints()
 
-    print "%s (%d points)" % (pfx, pts)
+            print "%s (%d points)" % (pfx, pts)
 
-    veldata = fdata.GetPointData().GetArray('Velocity')
+            veldata = fdata.GetPointData().GetArray('Velocity')
 
-    linf = 0
-    l1 = 0
-    l2 = 0
+            linf = 0
+            l1 = 0
+            l2 = 0
 
-    if gnuplot:
-        print "z\tvel\ttheory\terror\terrpct"
+            if gnuplot:
+                print "z\tvel\ttheory\terror\terrpct"
 
-    for pt in range(0, pts):
-        pos = fdata.GetPoint(pt)
-        vel = veldata.GetTuple(pt)
+            for pt in range(0, pts):
+                pos = fdata.GetPoint(pt)
+                vel = veldata.GetTuple(pt)
 
-        z = pos[2]
-        vel_theory = ( compute_poiseuille_vel(z), 0, 0 )
+                z = pos[2]
+                vel_theory = ( compute_poiseuille_vel(z), 0, 0 )
 
-        err = tuple(abs(a - b) for a, b in zip(vel, vel_theory))
-        err_x = err[0]
+                err = tuple(abs(a - b) for a, b in zip(vel, vel_theory))
+                err_x = err[0]
 
-        errpct = err_x*100/(vel_theory[0])
+                errpct = err_x*100/(vel_theory[0])
 
-        linf = max(linf, err_x)
-        l1 += err_x
-        l2 += err_x*err_x
+                linf = max(linf, err_x)
+                l1 += err_x
+                l2 += err_x*err_x
 
-        if gnuplot:
-            print "%g\t%g\t%g\t%g\t%g" % (z, vel[0], vel_theory[0], err[0], errpct)
+                if gnuplot:
+                    print "%g\t%g\t%g\t%g\t%g" % (z, vel[0], vel_theory[0], err[0], errpct)
 
-    l1 = l1/pts
-    l2 = math.sqrt(l2/pts)
-    print "%s[%d] linf\tl1\tl2" % (pfx, ppH)
-    print "%s%.2e\t%.2e\t%.2e" % (pfx, linf, l1, l2)
+            l1 = l1/pts
+            l2 = math.sqrt(l2/pts)
+            print "%s[%s,%s,%d] linf\tl1\tl2" % (pfx, compvisc, avgop, ppH)
+            print "%s%.2e\t%.2e\t%.2e" % (pfx, linf, l1, l2)
 
-    errors.append([ppH, linf, l1, l2])
+            errors.append([compvisc, avgop, ppH, linf, l1, l2])
 
-print "%sres\tlinf\tl1\tl2" % (pfx,)
+print "%scompvisc\tavgop\tres\tlinf\tl1\tl2" % (pfx,)
 for data in errors:
-    print "%s%d\t%.2e\t%.2e\t%.2e" % (pfx, data[0], data[1], data[2], data[3])
+    print "%s%s\t%s\t%d\t%.2e\t%.2e\t%.2e" % (pfx, data[0], data[1], data[2], data[3], data[4], data[5])
