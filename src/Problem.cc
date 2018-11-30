2b547ccd9a41cebe71f413f3834bca43c0d689bd
Giuseppe Bilotta <giuseppe.bilotta@ingv.it>
2018-11-30 15:59:10 +0100

    Abort if particles are outside of the domain from the beginning
    We assume that the initial setup falls completely inside of the domain
    (from world origin to world origin + size), and that any particles not
    inside the domain are indicative of a setup error from the user.

--- src/Problem.cc
+++ src/Problem.cc
@@ -1371,6 +1371,18 @@ Problem::calc_grid_hash(int3 gridPos) const
 void
 Problem::calc_localpos_and_hash(const Point& pos, const particleinfo& info, float4& localpos, hashKey& hash) const
 {
+	// check if the particle is actually inside the domain
+	if (pos(0) < m_origin.x || pos(0) > m_origin.x + m_size.x ||
+		pos(1) < m_origin.y || pos(1) > m_origin.y + m_size.y ||
+		pos(2) < m_origin.z || pos(2) > m_origin.z + m_size.z)
+	{
+		uint pid = id(info);
+		stringstream errmsg;
+		errmsg << "Particle " << pid << " position " << make_double4(pos)
+			<< " is outside of the domain " << m_origin << "--" << (m_origin+m_size) ;
+		throw std::out_of_range(errmsg.str());
+	}
+
 	int3 gridPos = calc_grid_pos(pos);
 
 	// automatically choose between long hash (cellHash + particleId) and short hash (cellHash)
