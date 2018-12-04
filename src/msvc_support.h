c1ba023325340cf67fca2a969a63664e5144b491
Daniele Calanna <dev.daniele.calanna@gmail.com>
2018-12-04 12:52:13 +0100

    Define the NOMINMAX symbol at the top of your source, before you include any headers, to prevents Visual C++ defining min and max as macros somewhere in windows.h, to avoid interferences with the corresponding standard functions.

--- src/msvc_support.h
+++ src/msvc_support.h
@@ -3,6 +3,11 @@
 #ifndef _MSVC_SUPPORT
 #define _MSVC_SUPPORT
 
+// Define the NOMINMAX symbol at the top of your source, before you include any headers,
+// to prevents Visual C++ defining min and max as macros somewhere in windows.h,
+// to avoid interferences with the corresponding standard functions.
+#define NOMINMAX 
+
 #include <direct.h>
 #define mkdir(name, mode) _mkdir(name)
 
