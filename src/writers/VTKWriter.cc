GPUSPH/gpusph

commit e5bdc2fd8f1e618eff401e4220f428bd2bbe1f57

VTKWriter: wavegages and planes are still unstructured grids
We should probably convert them to PolyData too, but at least for the
time being let's just give the file the correct extensions.

src/writers/VTKWriter.cc
@@ -711,7 +711,7 @@ void
 VTKWriter::write_WaveGage(double t, GageList const& gage)
 {
 	ofstream fp;
-	string filename = open_data_file(fp, "WaveGage", current_filenum());
+	string filename = open_data_file(fp, "WaveGage", current_filenum(), "vtu");
 
 	size_t num = gage.size();
 
@@ -794,7 +794,7 @@ void
 VTKWriter::save_planes()
 {
 	ofstream fp;
-	m_planes_fname = open_data_file(fp, "PLANES");
+	m_planes_fname = open_data_file(fp, "PLANES", "", "vtu");
 
 	fp << set_vector_fmt(" ");
 

commit c5e4001e81036fcdfcdcaa9501f03fc7f603e55b

Forgot dot in vtu extension
Bug introduced in e5bdc2fd8f1e618eff401e4220f428bd2bbe1f57

src/writers/VTKWriter.cc
@@ -711,7 +711,7 @@ void
 VTKWriter::write_WaveGage(double t, GageList const& gage)
 {
 	ofstream fp;
-	string filename = open_data_file(fp, "WaveGage", current_filenum(), "vtu");
+	string filename = open_data_file(fp, "WaveGage", current_filenum(), ".vtu");
 
 	size_t num = gage.size();
 
@@ -794,7 +794,7 @@ void
 VTKWriter::save_planes()
 {
 	ofstream fp;
-	m_planes_fname = open_data_file(fp, "PLANES", "", "vtu");
+	m_planes_fname = open_data_file(fp, "PLANES", "", ".vtu");
 
 	fp << set_vector_fmt(" ");
 