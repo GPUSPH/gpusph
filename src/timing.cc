70b9d6544d53eccb49d908d38f408ed8eaeef972
Daniele Calanna <dev.daniele.calanna@gmail.com>
2018-12-05 17:14:21 +0100

    IPPSCounter now uses std::chrono

--- src/timing.cc
+++ src/timing.cc
@@ -22,22 +22,3 @@
     You should have received a copy of the GNU General Public License
     along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-/*! \file
- * Timing info implementation
- */
-
-#include "timing.h"
-
-#ifdef __APPLE__
-//! Pseudo-implementation of clock_gettime() on Mac OS X
-//! NOTE: assuming this behaves similarly to CLOCK_MONOTONIC. This should be tested on a multi-GPU mac.
-int clock_gettime(int /*clk_id*/, struct timespec* t) {
-	struct timeval now;
-	int rv = gettimeofday(&now, NULL);
-	if (rv) return rv;
-	t->tv_sec  = now.tv_sec;
-	t->tv_nsec = now.tv_usec * 1000;
-	return 0;
-}
-#endif
