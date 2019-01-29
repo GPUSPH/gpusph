0abe49f6b96ad3fccf834d7d4bd178d089e4d1eb
Giuseppe Bilotta <giuseppe.bilotta@ingv.it>
2019-01-29 15:25:05 +0100

    INTEGRATE_GAMMA: use particle state selection

--- src/GPUWorker.cc
+++ src/GPUWorker.cc
@@ -2397,14 +2397,34 @@ void GPUWorker::runCommand<INTEGRATE_GAMMA>()
 	// is the device empty? (unlikely but possible before LB kicks in)
 	if (numPartsToElaborate == 0) return;
 
-	const int step = get_step_number(gdata->commandFlags);
+	const flag_t step_flag = gdata->commandFlags & ALL_INTEGRATION_STEPS;
+	const int step = get_step_number(step_flag);
 	const bool firstStep = (step == 1);
 
-	BufferList &bufwrite = m_dBuffers.getWriteBufferList();
+	// Integration always uses the step n state for starters,
+	// and will write to step n* or step n+1
+	const string base_state = "step n";
+	const string next_state = (step == 1 ? "step n*" : "step n+1");
+	// the forces were computed in the base state for the predictor,
+	// on the next state for the corrector
+	const string forces_state = (step == 1 ? base_state : next_state);
+
+	const BufferList bufread = m_dBuffers.state_subset(base_state,
+		BUFFER_POS | BUFFER_HASH | BUFFER_INFO | BUFFER_CELLSTART | BUFFER_NEIBSLIST |
+		BUFFER_VEL |
+		BUFFER_VERTPOS | BUFFER_EULERVEL | BUFFER_GRADGAMMA | BUFFER_BOUNDELEMENTS);
+
+	BufferList bufwrite = m_dBuffers.state_subset(next_state,
+		BUFFER_POS /* this is only accessed for reading */ |
+		BUFFER_EULERVEL /* this is only accessed for reading */ |
+		BUFFER_BOUNDELEMENTS /* this is only accessed for reading */ |
+		BUFFER_VEL /* this is only accessed for reading */ |
+		BUFFER_GRADGAMMA);
+
 	bufwrite.add_manipulator_on_write("integrateGamma" + to_string(step));
 
 	integrationEngine->integrate_gamma(
-		m_dBuffers.getReadBufferList(),	// this is the read only arrays
+		bufread,
 		bufwrite,
 		m_numParticles,
 		numPartsToElaborate,
