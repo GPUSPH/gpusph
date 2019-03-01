c1e50223bc896eb342b17fce3c3336103b900a9b
Giuseppe Bilotta <giuseppe.bilotta@ingv.it>
2019-03-01 20:59:44 +0100

    Introduce common params
    There are several substructures that are shared by many kernel parameter
    template structures. Let us refactor them.

---
+++ src/cuda/common_params.h
@@ -0,0 +1,92 @@
+/*  Copyright 2019 Giuseppe Bilotta, Alexis Herault, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+
+    Istituto Nazionale di Geofisica e Vulcanologia
+        Sezione di Catania, Catania, Italy
+
+    Università di Catania, Catania, Italy
+
+    Johns Hopkins University, Baltimore, MD
+
+    This file is part of GPUSPH.
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef COMMON_PARAMS_H
+#define COMMON_PARAMS_H
+
+/* \file
+ *
+ * With many kernels adopting the conditional structure template for parameters, we want
+ * to simplify their usage by refactoring common substructures and avoid duplicating the initialization
+ * sequences.
+ *
+ * This file collects params structure designed to be used as (sub)components of the actual kernel params
+ * structures.
+ */
+
+#include "cond_params.h"
+
+/*! \ingroup Common integration structures
+ *
+ * These are structures that hold two copies (old and new) of the same array. The first is assumed
+ * to always be constant, whereas the second can be const or not, depending on a boolean template parameter.
+ * @{
+ */
+
+template<bool B, typename T>
+using writable_type = typename std::conditional<B, T, const T>::type;
+
+/* Since they all have the same structure, we use a macro to define them */
+
+#define DEFINE_PAIR_PARAM(type, member, buffer) \
+template<bool writable_new = true> struct member ## _params { \
+	using newType = writable_type<writable_new, type>; \
+	using writeBufType = writable_type<writable_new, BufferList>; \
+	const type * __restrict__ old ## member; \
+	newType * __restrict__ new ## member; \
+	member ## _params(BufferList const& bufread, \
+		writeBufType& bufwrite) \
+	: \
+		old ## member(bufread.getData<buffer>()), \
+		new ## member(bufwrite.template getData<buffer>()) \
+	{} \
+}
+
+// Pos_params oldPos, newPos
+DEFINE_PAIR_PARAM(float4, Pos, BUFFER_POS);
+
+// Vel_params oldVel, newVel
+DEFINE_PAIR_PARAM(float4, Vel, BUFFER_VEL);
+
+// EulerVel_params oldEulerVel, newEulerVel
+DEFINE_PAIR_PARAM(float4, EulerVel, BUFFER_EULERVEL);
+
+// Vol_params oldVol, newVol
+DEFINE_PAIR_PARAM(float4, Vol, BUFFER_VOLUME);
+
+// BoundElement_params oldBoundElement, newBoundElement
+DEFINE_PAIR_PARAM(float4, BoundElement, BUFFER_BOUNDELEMENTS);
+
+// Eps_params oldEps, newEps
+DEFINE_PAIR_PARAM(float, Eps, BUFFER_EPSILON);
+
+// TKE_params oldTKE, newTKE
+DEFINE_PAIR_PARAM(float, TKE, BUFFER_TKE);
+
+// Energy_params oldEnergy, newEnergy
+DEFINE_PAIR_PARAM(float, Energy, BUFFER_INTERNAL_ENERGY);
+/*! @} */
+
+#endif
