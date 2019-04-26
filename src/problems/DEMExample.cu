9fe07bacf9faf9aba6a549bf3d1827e0dd281d72
Giuseppe Bilotta <giuseppe.bilotta@ingv.it>
2019-04-26 08:48:45 +0200

    DEMExample: sample problems show the use of a topography
    It's just a still-water problem, but it could be expanded to include a
    wave generator.

---
+++ src/problems/DEMExample.cu
@@ -0,0 +1,68 @@
+/*  Copyright 2019 Giuseppe Bilotta, Alexis Herault, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+
+    Istituto Nazionale di Geofisica e Vulcanologia
+        Sezione di Catania, Catania, Italy
+
+    Università di Catania, Catania, Italy
+
+    Johns Hopkins University, Baltimore, MD
+
+    This file is part of GPUSPH.
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Example usage of DEM with Problem API 1 aka XProblem
+
+#include <string>
+
+#include "DEMExample.h"
+#include "cudasimframework.cu"
+
+using namespace std;
+
+DEMExample::DEMExample(GlobalData *gdata) : Problem(gdata)
+{
+	const int mlsIters = get_option("mls", 0); // --mls N to enable MLS filter every N iterations
+	const int ppH = get_option("ppH", 16); // --ppH N to change deltap to H/N
+
+	// density diffusion terms, see DensityDiffusionType
+	const DensityDiffusionType rhodiff = get_option("density-diffusion", DENSITY_DIFFUSION_NONE);
+
+	const string dem_file = get_option("dem", "half_wave0.1m.txt");
+
+	SETUP_FRAMEWORK(
+		viscosity<ARTVISC>,
+		boundary<LJ_BOUNDARY>,
+		add_flags<ENABLE_DEM | ENABLE_PLANES>
+	).select_options(rhodiff);
+
+	/* Simulation parameters */
+	set_deltap(0.05);
+	double water_height = 0.8;
+
+	/* Physical parameters */
+	setMaxFall(2.0);
+	set_gravity(-9.81f);
+	size_t water = add_fluid(1000.0f);
+	set_equation_of_state(water, 7.0f, NAN /* autocompute from max fall */);
+
+	/* Geometries */
+	GeometryID dem = addDEM(dem_file);
+	GeometryID fluid_box = addDEMFluidBox(water_height);
+	vector<GeometryID> planes = addDEMPlanes();
+
+	add_writer(VTKWRITER, 0.1);
+}
+
---
+++ src/problems/DEMExample.h
@@ -0,0 +1,35 @@
+/*  Copyright 2019 Giuseppe Bilotta, Alexis Herault, Robert A. Dalrymple, Eugenio Rustico, Ciro Del Negro
+
+    Istituto Nazionale di Geofisica e Vulcanologia
+        Sezione di Catania, Catania, Italy
+
+    Università di Catania, Catania, Italy
+
+    Johns Hopkins University, Baltimore, MD
+
+    This file is part of GPUSPH.
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Example usage of DEM with Problem API 1 aka XProblem
+
+#define PROBLEM_API 1
+#include "Problem.h"
+
+class DEMExample : public Problem
+{
+public:
+	DEMExample(GlobalData *);
+};
