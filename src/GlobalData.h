4920b033b41e052094ce3f084245954372a8e80f
alexGhts <alex.ghaitanellis@gmail.com>
2019-05-27 19:43:57 +0800

    Jacobi global variables are now floats.

--- src/GlobalData.h
+++ src/GlobalData.h
@@ -271,8 +271,10 @@ struct GlobalData {
 	uint*   h_maxIOwaterdepth;
 
 	// Jacobi solver residual and backward error
-	float h_jacobiResidual[MAX_DEVICES_PER_NODE];
-	float h_jacobiBackwardError[MAX_DEVICES_PER_NODE];
+	float h_jacobiResidual;
+	float h_jacobiBackwardError;
+	bool h_jacobiStop;
+	int h_jacobiCounter;
 
 	// peer accessibility table (indexed with device indices, not CUDA dev nums)
 	bool s_hDeviceCanAccessPeer[MAX_DEVICES_PER_NODE][MAX_DEVICES_PER_NODE];
@@ -338,11 +340,10 @@ struct GlobalData {
 			dts[d] = 0.0F;
 
 		// init Jacobi solver auxiliary data
-		for (uint d=0; d < MAX_DEVICES_PER_NODE; d++) {
-			h_jacobiResidual[d] = NAN;
-			h_jacobiBackwardError[d] = NAN;
-		}
-
+		h_jacobiResidual = NAN;
+		h_jacobiBackwardError = NAN;
+		h_jacobiStop = false;
+		h_jacobiCounter = 0;
 		// init particlesCreatedOnNode
 		for (uint d=0; d < MAX_DEVICES_PER_NODE; d++)
 			particlesCreatedOnNode[d] = false;
