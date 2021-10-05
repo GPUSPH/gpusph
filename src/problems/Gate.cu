1207909065de0fb64f11a7e7b3e246d1a4463c63
Vito Zago <vito.zago@ingv.it>
2021-10-05 11:04:07 +0200

    Gate: update and clean up

--- src/problems/Gate.cu
+++ src/problems/Gate.cu
@@ -48,7 +48,6 @@ Gate::Gate(GlobalData *_gdata) : XProblem(_gdata)
 	// * = tested in this problem
 	SETUP_FRAMEWORK(
 		kernel<WENDLAND>,
-		viscosity<DYNAMICVISC>,
 		boundary<DYN_BOUNDARY>,
 		periodicity<PERIODIC_Y>,
 		add_flags<ENABLE_FEA>
@@ -56,36 +55,25 @@ Gate::Gate(GlobalData *_gdata) : XProblem(_gdata)
 		RHODIFF
 	);
 
-
-	// Allow user to set the MLS frequency at runtime. Default to 0 if density
-	// diffusion is enabled, 10 otherwise
-	const int mlsIters = get_option("mls",
-		(simparams()->densitydiffusiontype != DENSITY_DIFFUSION_NONE) ? 0 : 10);
-
-	if (mlsIters > 0)
-		addFilter(MLS_FILTER, mlsIters);
-
 	// Explicitly set number of layers. Also, prevent having undefined number of layers before the constructor ends.
 	setDynamicBoundariesLayers(3);
 
-	//resize_neiblist(128);
-
 	// *** Initialization of minimal physical parameters
-	//set_deltap(1/512.0f);
-	set_deltap(0.0005);
+	set_deltap(0.001);
 	physparams()->r0 = m_deltap;
 	physparams()->gravity = make_float3(0.0, 0.0, -9.81);
 	const float g = length(physparams()->gravity);
 	H = 0.14; //water height
 	const double GateL = 0.08; //Gate height
-	physparams()->dcoeff = 10.0f * g * H;
+	physparams()->dcoeff = 10.0f*g*H;
 	water = add_fluid(1000.0);
 
-	set_equation_of_state(water,  7.0f, 20.0*sqrt(2*9.81*H));
+	set_equation_of_state(water, 7.0f, 20.0*sqrt(2*9.81*H));
 	set_kinematic_visc(0, 1.0e-6f);
-
-	simparams()->tend=0.4f;
+	physparams()->artvisccoeff = 0.025;
 	simparams()->densityDiffCoeff = 0.1f;
+
+	simparams()->tend = 0.4f;
 
 	// Drawing and saving times
 	add_writer(VTKWRITER, 0.005f);
@@ -99,24 +87,13 @@ Gate::Gate(GlobalData *_gdata) : XProblem(_gdata)
 	const double water_length = 0.1;
 	const double water_height = H;
 
-	// Planes unfill automatically but the box won't, to void deleting all the water. Thus,
-	// we define the water at already the right distance from the walls.
 	double BOUNDARY_DISTANCE = m_deltap;
 	double BOUNDARY_THICKNESS = m_deltap;
-	if (simparams()->boundarytype == DYN_BOUNDARY) {
-			BOUNDARY_DISTANCE *= getDynamicBoundariesLayers(); // FIXME gives issues if number of layers not settled manually
-			BOUNDARY_THICKNESS *= (getDynamicBoundariesLayers() - 1);
-
-		/*BOUNDARY_DISTANCE *= 4;
-		BOUNDARY_THICKNESS *= 3;*/
+	if (simparams()->boundarytype == DYN_BOUNDARY || simparams()->boundarytype == DUMMY_BOUNDARY) {
+		BOUNDARY_DISTANCE *= getDynamicBoundariesLayers(); // FIXME gives issues if number of layers not settled manually
+		BOUNDARY_THICKNESS *= (getDynamicBoundariesLayers() - 1);
 	}
 
-	// If we used only makeUniverseBox(), origin and size would be computed automatically
-
-	/* // no periodicity
-	m_origin = make_double3(-BOUNDARY_DISTANCE, -BOUNDARY_DISTANCE, -BOUNDARY_DISTANCE);
-	m_size = make_double3(dimX + 2*BOUNDARY_DISTANCE, dimY + 2*BOUNDARY_DISTANCE, dimZ + 2*BOUNDARY_DISTANCE);
-	*/
 	m_origin = make_double3(0, m_deltap/2.0, 0);
 	m_size = make_double3(dimX, dimY - m_deltap, dimZ);
 
@@ -125,8 +102,7 @@ Gate::Gate(GlobalData *_gdata) : XProblem(_gdata)
 	// main container
 
 	GeometryID box = addBox(GT_FIXED_BOUNDARY, FT_BORDER, Point(-BOUNDARY_THICKNESS, -BOUNDARY_THICKNESS, -BOUNDARY_THICKNESS),
-		dimX + 2*BOUNDARY_THICKNESS, dimY + 2*BOUNDARY_THICKNESS, dimZ + 2*BOUNDARY_THICKNESS, 2, 5); // the last three integers are the number of fea elements in the three directions
-	// we simulate inside the box, so do not erase anything
+		dimX + 2*BOUNDARY_THICKNESS, dimY + 2*BOUNDARY_THICKNESS, dimZ + 2*BOUNDARY_THICKNESS, 2, 5); // the last two integers are the number of fea shell in the two directions
 	setEraseOperation(box, ET_ERASE_NOTHING);
 
 	// Add the main water part
@@ -151,9 +127,13 @@ Gate::Gate(GlobalData *_gdata) : XProblem(_gdata)
 
 
 	// Add the flexible gate as a mesh
-//	addTetFile(GT_DEFORMABLE_BODY, FT_BORDER, Point(water_length, m_deltap, m_deltap), "gate_001.node", "gate_001.ele", GateL + m_deltap*0.5) ;
-	GeometryID gate = addBox(GT_DEFORMABLE_BODY, FT_BORDER, Point(water_length, m_deltap, m_deltap), 0.005, dimY - 2*m_deltap, GateL, 1, 10);
+	GeometryID gate = addBox(GT_DEFORMABLE_BODY, FT_BORDER, Point(water_length + 0.005, m_deltap, m_deltap), GateL, dimY - 2*m_deltap, round_up(0.005, m_deltap), 10, 1);
 	setEraseOperation(gate, ET_ERASE_NOTHING);
+	setYoungModulus(gate, 1e7);
+	setPoissonRatio(gate, 0.3);
+	setAlphaDamping(gate, 0.001);
+	setDensity(gate, 1100);
+	rotate(gate, 0, M_PI/2, 0);
 	set_fea_ground(0, 0, -1, -(GateL + 0.5*m_deltap)); // a, b, c and d parameters of a plane equation. Grounding nodes in the negative side of the plane
 }
 
@@ -163,6 +143,7 @@ Gate::Gate(GlobalData *_gdata) : XProblem(_gdata)
 	fillDeviceMapByAxis(Y_AXIS);
 }
 */
+
 void Gate::initializeParticles(BufferList &buffer, const uint numParticle)
 {
 	float4 *pos = buffer.getData<BUFFER_POS>();
@@ -170,14 +151,11 @@ void Gate::initializeParticles(BufferList &buffer, const uint numParticle)
 	ushort4 *info= buffer.getData<BUFFER_INFO>();
 	double4 *pos_global = buffer.getData<BUFFER_POS_GLOBAL>();
 
-	// TODO FIXME the particle mass should be assigned from the mesh. We should 
-	// understand why GetMass on the fea mesh gives 0
-
 	for (uint i = 0; i < numParticle; i++) {
 
 		double depth = 3*m_deltap +  H - pos_global[i].z + m_origin.z;
 
-		pos[i].w = 1000*m_deltap*m_deltap*m_deltap;
+		pos[i].w = physparams()->rho0[0]*m_deltap*m_deltap*m_deltap;
 		vel[i].w = hydrostatic_density(depth, water);
 	}
 }
