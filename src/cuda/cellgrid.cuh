f6312ff5930e044308f6f8f9ba0249ea0b91a80c
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2021-11-01 16:06:26 +0100

    Remove duplicate macros

--- src/cuda/cellgrid.cuh
+++ src/cuda/cellgrid.cuh
@@ -46,18 +46,6 @@
 namespace cuneibs
 {
 
-/* The neighbor cell num ranges from 1 to 27 (included), so it fits in
- * 5 bits, which we put in the upper 5 bits of the neibdata, which is
- * 16-bit wide.
- * TODO actually compute this from sizeof(neibdata)
- */
-#define CELLNUM_SHIFT	11
-#define CELLNUM_ENCODED	(1U<<CELLNUM_SHIFT)
-#define NEIBINDEX_MASK	(CELLNUM_ENCODED-1)
-#define ENCODE_CELL(cell) ((cell + 1) << CELLNUM_SHIFT)
-#define DECODE_CELL(data) ((data >> CELLNUM_SHIFT) - 1)
-
-
 /** \addtogroup cellgrid Common cell/grid related device functions and variables/constants
  * 	\ingroup neibs
  * 	Device constant/variables and functions involved in computing particles hash, relative positions
