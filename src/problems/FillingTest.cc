65597c20817e27a91c5ea14ac46eb7d01c3ee612
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2021-11-06 12:08:36 +0100

    FillingTest problem
    This can be used to test (and debug) how each geometry behaves under
    several filling conditions.

---
+++ src/problems/FillingTest.cc
@@ -0,0 +1,85 @@
+/*  Copyright (c) 2021 INGV, EDF, UniCT, JHU
+
+    Istituto Nazionale di Geofisica e Vulcanologia, Sezione di Catania, Italy
+    Électricité de France, Paris, France
+    Università di Catania, Catania, Italy
+    Johns Hopkins University, Baltimore (MD), USA
+
+    This file is part of GPUSPH. Project founders:
+        Alexis Hérault, Giuseppe Bilotta, Robert A. Dalrymple,
+        Eugenio Rustico, Ciro Del Negro
+    For a full list of authors and project partners, consult the logs
+    and the project website <https://www.gpusph.org>
+
+    GPUSPH is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GPUSPH is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GPUSPH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "FillingTest.h"
+#include "GlobalData.h"
+
+/* The purpose of the FillingTest problem is simply to generate a number of geometries
+ * and see how they get filled. It is intended to be used to validate filling patterns. 
+ */
+
+FillingTest::FillingTest(GlobalData *gdata) :
+	Problem(gdata)
+{
+	setup_framework();
+	// we only care about seeing the result
+	gdata->maxiter = 1;
+
+	// No external forces
+	set_gravity(0.0);
+
+	// Fluid with unitary density;
+	auto fluid = add_fluid(1.0);
+	set_equation_of_state(fluid, 1.0, 1.0);
+
+	// avoid rounding issues and make it easy to debug also in decimal
+	set_deltap(1.0);
+
+	// The basics: a filled box.
+	const double box_side = 16.0;
+
+	// The default filling method is “border-centered”, in the sense that the particles of the first layer
+	// have their centers on the border.
+	// This requires us to shift the geometries so that they don't interfere with each other
+	const double shift = m_deltap/2;
+	const double double_shift = m_deltap; // 2*shift, needed to fix up the length of the box
+
+	// We also have to take positioning into consideration. The default is PP_CENTER, meaning that
+	// for the box we don't need to do any shifting. We set up two boxes,
+	// one with PP_CENTER, and one with PP_CORNER positioning policies.
+	// The PP_CORNER one will be placed with corner in (0, 0, 0),
+	// while the PP_CENTER one will be placed so that its center is 2*box_side away
+	// from the PP_CORNER case along the y direction (without accounting for the shift!)
+
+	const Point shifted_corner  = Point(shift, shift, shift);
+	const Point centered_center = Point(box_side/2, 2*box_side, box_side/2);
+
+	// PP_CENTER
+	auto fluid_box_centered = addCube(GT_FLUID, FT_SOLID, centered_center, box_side - double_shift);
+	setEraseOperation(fluid_box_centered, ET_ERASE_NOTHING);
+
+	auto border_box_centered = addCube(GT_FIXED_BOUNDARY, FT_OUTER_BORDER, centered_center, box_side + double_shift);
+	setEraseOperation(border_box_centered, ET_ERASE_NOTHING);
+
+	setPositioning(PP_CORNER);
+	auto fluid_box_corner = addCube(GT_FLUID, FT_SOLID, shifted_corner, box_side - double_shift);
+	setEraseOperation(fluid_box_corner, ET_ERASE_NOTHING);
+
+	auto border_box_corner = addCube(GT_FIXED_BOUNDARY, FT_OUTER_BORDER, -shifted_corner, box_side + double_shift);
+	setEraseOperation(border_box_corner, ET_ERASE_NOTHING);
+}
+
