4eb08bef9e476e4fdfc3f35be9ea7fdc15ff20f0
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2021-11-12 17:38:46 +0100

    BuoyancyTest: rewrite
    Redefine geometries leveraging the new filling method, and recover
    support for alternative floater objects (cube, sphere, torus).
    Also add support for the --ppH command-line option.

--- src/problems/BuoyancyTest.cu
+++ src/problems/BuoyancyTest.cu
@@ -38,13 +38,10 @@
 BuoyancyTest::BuoyancyTest(GlobalData *_gdata) : Problem(_gdata)
 {
 	// Size and origin of the simulation domain
-	double lx = 1.0;
-	double ly = 1.0;
-	double lz = 1.0;
-	double H = 0.6;
-
-	//m_size = make_double3(lx, ly, lz);
-	//m_origin = make_double3(0.0, 0.0, 0.0);
+	const double lx = 1.0;
+	const double ly = 1.0;
+	const double lz = 1.0;
+	const double H = 0.6;
 
 	SETUP_FRAMEWORK(
 		kernel<WENDLAND>,
@@ -54,63 +51,52 @@ BuoyancyTest::BuoyancyTest(GlobalData *_gdata) : Problem(_gdata)
 		boundary<DYN_BOUNDARY>
 	);
 
-	// reduce autocomputed number of layers
-	setDynamicBoundariesLayers(3);
+	const int ppH = get_option("ppH", 16);
 
 	// SPH parameters
-	set_deltap(0.04); //0.008
-	set_timestep(0.0003f);
+	set_deltap(H/ppH);
 	simparams()->dtadaptfactor = 0.3;
 	simparams()->buildneibsfreq = 10;
-	simparams()->tend = 5.0f; //0.00036f
+	simparams()->tend = 8;
 
 	// Physical parameters
 	set_gravity(-9.81f);
 	setMaxFall(H);
 
-	add_fluid(1000.0);
-	set_equation_of_state(0,  7.0f, 20.f);
-
+	auto water = add_fluid(1000.0);
+	set_equation_of_state(water,  7.0f, NAN /* autocompute speed of sound */);
 
 	set_kinematic_visc(0, 1.0e-6f);
 
 	add_writer(VTKWRITER, 0.1);
 
-	const double offs = m_deltap * getDynamicBoundariesLayers();
-	//addExtraWorldMargin(2*offs);
+	setPositioning(PP_BOTTOM_CENTER);
+	setFillingMethod(Object::BORDER_TANGENT);
 
-	setPositioning(PP_CORNER);
+	const Point bottom_center(0, 0, 0);
 
-	// DYN fill is inwards, so when using a cube as a container must take care of offsets
-	GeometryID cube = addBox(GT_FIXED_BOUNDARY, FT_BORDER, Point(0,0,0),
-		lx + 2 * offs, ly + 2 * offs, lz + 2 * offs);
-	disableCollisions(cube);
+	GeometryID domain_box = addBox(GT_FIXED_BOUNDARY, FT_OUTER_BORDER,
+		bottom_center, lx, ly, lz);
+	disableCollisions(domain_box);
+	// cut off the top of the domain box
+	addPlane(0, 0, -1, lz, FT_UNFILL);
 
-	addBox(GT_FLUID, FT_SOLID, Point(offs, offs, offs), lx, ly, H);
+	addBox(GT_FLUID, FT_SOLID, bottom_center, lx, ly, H);
 
-	// TODO
-	/*
-	switch (object_type) {
-		case 0: {
-			olx, oly, olz = 10.0*m_deltap;
-			cube  = Cube(Point(lx/2.0 - olx/2.0, ly/2.0 - oly/2.0, H/2.0 - olz/2.0), olx, oly, olz);
-		case 1: {
-			double r = 6.0*m_deltap;
-			sphere = Sphere(Point(lx/2.0, ly/2.0, H/2.0 - r/4.0), r);
-		case 2: // TORUS
-	*/
-	/*
-	double R = lx * 0.2;
-	double r = 4.0 * m_deltap;
-	GeometryID torus = addTorus(GT_FLOATING_BODY, FT_BORDER, Point(lx/2.0, ly/2.0, H/2.0), R, r);
-	setMassByDensity(torus, physparams()->rho0[0]*0.5);
-	*/
 	setPositioning(PP_CENTER);
-	const double SIDE = lx * 0.4;
-	GeometryID floating_cube = addCube(GT_FLOATING_BODY, FT_BORDER,
-		Point(offs + lx/2.0, offs + ly/2.0, offs + H/2.0), SIDE);
-	setMassByDensity(floating_cube, physparams()->rho0[0]*0.5);
+	const double floater_side = lx *0.4;
+	const Point floater_center = bottom_center + Vector(0, 0, H/2);
+	const std::string floater_type = get_option("floater", "cube");
+	GeometryID floater;
 
-	// Name of problem used for directory creation
-	m_name = "BuoyancyTest";
+	if (floater_type == "cube") {
+		floater = addCube(GT_FLOATING_BODY, FT_INNER_BORDER, floater_center, floater_side);
+	} else if (floater_type == "sphere") {
+		floater = addSphere(GT_FLOATING_BODY, FT_INNER_BORDER, floater_center, floater_side/2);
+	} else if (floater_type == "torus") {
+		floater = addTorus(GT_FLOATING_BODY, FT_INNER_BORDER, floater_center, floater_side/2, floater_side/4);
+	}
+
+	setMassByDensity(floater, physparams()->rho0[0]*0.5);
+
 }
