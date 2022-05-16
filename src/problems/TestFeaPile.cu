adbeff139bd8910ed3989b1aa189650d3b5b2b21
Vito Zago <vito.zago@ingv.it>
2022-05-15 18:26:23 -0700

    TestFeaPile: clean up and update

--- src/problems/TestFeaPile.cu
+++ src/problems/TestFeaPile.cu
@@ -35,13 +35,15 @@
 #include "GlobalData.h"
 #include "cudasimframework.cu"
 
-/*0 load from TetFile, 1: define single Cylinder, 2: Four legged Platform square pillers, 3:Four legged platform Cylindrical pillers*/
-
 #define USE_WATER 0
 
 
 #define MK_par 2
-
+/* Simple problem to test the FEM model in GPUSPH.
+   A pile hinged at the bottom and laterally pulled at its top.
+   Displacements of the top end and reaction forces at the base
+   are recorded.
+ */
 TestFeaPile::TestFeaPile(GlobalData *_gdata) : XProblem(_gdata)
 {
 	// Size and origin of the simulation domain
@@ -56,14 +58,12 @@ TestFeaPile::TestFeaPile(GlobalData *_gdata) : XProblem(_gdata)
 	m_size = make_double3(lx, ly, lz);
 	m_origin = make_double3(-lx/2.0, -ly/2.0, -0.5);
 
-
 	// SPH parameters
 	set_deltap(1/64.0);
 	simparams()->dtadaptfactor = 0.2;
 	simparams()->buildneibsfreq = 10;
 
 	simparams()->t_fea_start= 0.0f; //seconds
-
 	simparams()->fcallback = true;
 
 	// Physical parameters
@@ -75,84 +75,84 @@ TestFeaPile::TestFeaPile(GlobalData *_gdata) : XProblem(_gdata)
 	physparams()->r0 = r0;
 
 	add_fluid( 1000.0f);
-	set_equation_of_state(0,  7.0f, 80.f);
-	set_kinematic_visc(0,1.0e-6);
+	set_equation_of_state(0,  7.0f, NAN);
+	simparams()->fea_write_every = 0.01f;
 
-	physparams()->artvisccoeff =  0.2;
-	physparams()->smagfactor = 0.12*0.12*m_deltap*m_deltap;
-	physparams()->kspsfactor = (2.0/3.0)*0.0066*m_deltap*m_deltap;
-	physparams()->epsartvisc = 0.01*simparams()->slength*simparams()->slength;
-
-	// BC when using LJ
-	physparams()->dcoeff = 0.5*g*H;
-	//set p1coeff,p2coeff, epsxsph here if different from 12.,6., 0.5
-
-	add_writer(VTKWRITER, 0.005);  //second argument is saving time in seconds
+	add_writer(VTKWRITER, 0.1);  //second argument is saving time in seconds
 
 	// Name of problem used for directory creation
 	m_name = "TestFeaPile";
 
-	// Building the geometry
-	setPositioning(PP_CORNER);
-/*
-	if (H > 0) {
-		GeometryID fluid = addBox(GT_FLUID, FT_SOLID,
-			Point(-lx/2.0, -ly/2.0, 0), lx, ly, H);
-	}
-*/
-	setPositioning(PP_CORNER);
-
-	set_fea_ground(0, 0, 1, -0.5); // a, b, c and d parameters of a plane equation. Grounding nodes in the negative side of the plane
-
 	double inner_radius = 0.065;
 	double outer_radius = 0.1;
 	double height = 3;
-	int num_els = 1;
+	int num_els = 3;
 
-	GeometryID pile = addCylinder(GT_DEFORMABLE_BODY, FT_BORDER, Point(-outer_radius, -outer_radius, 0), outer_radius, inner_radius, height, num_els);
+	// Building the geometry
+	setPositioning(PP_BOTTOM_CENTER);
+	GeometryID pile = addCylinder(GT_DEFORMABLE_BODY, FT_INNER_BORDER, Point(0.0, 0.0, 0.0), outer_radius, inner_radius, height, num_els);
 	setEraseOperation(pile, ET_ERASE_FLUID);
 
 	setYoungModulus(pile, 1e8);
 	setPoissonRatio(pile, 0.3);
+	setAlphaDamping(pile, 0.1);
 	setDensity(pile, 1522.0);
 
-	GeometryID force_box = addBox(GT_FEA_FORCE, FT_BORDER, Point(-0.05, -0.05, height - 0.05), 0.1, 0.1, 0.1);
-	GeometryID write_box = addBox(GT_FEA_WRITE, FT_BORDER, Point(-0.05, -0.05, height - 0.05), 0.1, 0.1, 0.1);
+	setPositioning(PP_CORNER);
+
+	// Forces are applied to FEA nodes embedded in the GT_FEA_FORCE geometry
+	GeometryID force_box = addBox(GT_FEA_FORCE, FT_NOFILL, Point(-0.05, -0.05, height - 0.05), 0.1, 0.1, 0.1);
+
+	// Positoins are recorded for FEA nodes embedded in the GT_FEA_WRITE geometry
+	GeometryID write_box = addBox(GT_FEA_WRITE, FT_NOFILL, Point(-0.05, -0.05, height - 0.05), 0.1, 0.1, 0.1);
+
+
+	// Two ways of fixing the base of the pile:
+#if 1
+	// No reaction forces measured
+	set_fea_ground(0, 0, 1, 0.1); // a, b, c and d parameters of a plane equation. Grounding nodes in the negative side of the plane
+#else
+	// Measuring reaction forces
+
 	setPositioning(PP_CENTER);
-	GeometryID joint_box = addBox(GT_FEA_RIGID_JOINT, FT_BORDER, Point(-0.0, -0.0, - 0.05), 0.1, 0.1, 0.2);
-	simparams()->fea_write_every = 0.01f;
+
+	// Join all the nodes that fall inside the GT_FEA_RIGID_JOINT
+	GeometryID load_cell = addBox(GT_FEA_RIGID_JOINT, FT_NOFILL, Point(-0.0, -0.0, - 0.05), 0.1, 0.1, 0.2);
+
+	// Enable recording reaction forces in the joint
+	setDynamometer(load_cell, true);
+#endif
 }
 
 float3 TestFeaPile::ext_force_callback(const double t)
 {
-	/* NOTE: set simparams::fcallback to true */
 	float forcex;
 
+	/* Gradually apply a force to pull the pile along the x-axis, then suddently release it at t = 6s */
 	if (t < 6.0)
-		forcex = 50/(1 + exp(2*(2.5 - t))); // logistic function with L = 100, k = 3; x0 = 2 
+		forcex = 50/(1 + exp(2*(2.5 - t))); // logistic function with L = 50, k = 2; x0 = 2.5 
 	else
 		forcex = 0;
 
 	return make_float3(forcex, 0.0, 0.0);
 }
 
+// Use this function to manually initialize particle properties
 void TestFeaPile::initializeParticles(BufferList &buffer, const uint numParticle)
 {
 	float4 *pos = buffer.getData<BUFFER_POS>();
 	const float4 *vel = buffer.getData<BUFFER_VEL>();
 	const ushort4 *info= buffer.getData<BUFFER_INFO>();
 
-	// TODO FIXME the particle mass should be assigned from the mesh. We should 
-	// understand why GetMass on the fea mesh gives 0
-
 	for (uint i = 0; i < numParticle; i++) {
-		if (DEFORMABLE(info[i]))
-			pos[i].w = physical_density(vel[i].w, 0)*m_deltap*m_deltap*m_deltap;
-	}
+			// Do nothing
+		}
 }
 
+// Request write
 bool TestFeaPile::need_write(double t) const
 {
+	// Save at every iteration if returning true
 	return false;
 }
 #undef MK_par
